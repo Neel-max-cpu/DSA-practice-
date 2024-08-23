cd31479ab81f95a192cd7502e670e6a4f112f00a
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-08-23 13:30:09 +0530

    Time: 3 ms (72.67%), Space: 15 MB (37.83%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -33,6 +33,7 @@ public:
         //  multiset<int, greater<int> > -- decreasing
         map<int,map<int,multiset<int>>>m;
 
+        // level order traversal
         while(!q.empty()){
             // automatically dedeuce the data structure
             auto p = q.front();
@@ -45,18 +46,48 @@ public:
             m[x][y].insert(current->val);
 
             if(current->left != NULL){
-                q.push({current->left,{x-1, y+1}});                
+                // we want the vector to have verticall
+                q.push({current->left,{x-1, y+1}});    
+                
+                // we we do this we will get horizontally same col like 9,20 are in the same 1 but col is -1,1
+                // similarly 15,7 in the same 
+                /*
+                basically the vector will be like this
+                ----->
+                ----->
+                ----->
+
+                but we want the vector to be like this --
+                |  |  |
+                |  |  |
+                |  |  |
+                |  |  |
+                ↓  ↓  ↓
+                */          
+                // q.push({current->left,{x+1, y-1}});                
             }
             if(current->right!=NULL){
                 q.push({current->right,{x+1, y+1}});                
+                // q.push({current->right,{x+1, y+1}});                
             }
         }
+        
 
+        /*
+            -1: {1: {9}},               // Vertical distance -1: Level 1 -> Node 9
+            0: {0: {3}, 2: {15}},      // Vertical distance 0: Level 0 -> Node 3, Level 2 -> Node 15
+            1: {1: {20}},              // Vertical distance 1: Level 1 -> Node 20
+            2: {2: {7}}                  
+        */
         for(auto it:m){
             vector<int>col;    
             // move to second since int,multiset        
             for(auto x:it.second){
+                // and we have to push all the elements of each key 1 key might have mulitple multisets
                 // x.second cause second is multiset
+
+                //syntax =  vector_name.insert(position, iterator1, iterator2)
+                // since let say 0 ->1 = {} and 0->2={} and 0->3={} then i have to put all the values of 0 in the vector 
                 col.insert(col.end(), x.second.begin(), x.second.end());
             }
             ans.push_back(col);

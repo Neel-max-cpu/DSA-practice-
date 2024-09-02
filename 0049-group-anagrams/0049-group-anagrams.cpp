06c63003c013544d7aa81ec0cc07043c7cfdd39a
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-02 18:52:21 +0530

    Time: 75 ms (9.2%), Space: 38.6 MB (5.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -10,18 +10,35 @@ public:
         return ans;
     }
 
+    string getkey(string s){
+        vector<int>v(26);
+        for(int i=0; i<s.size(); i++){
+            v[s[i]-'a']++;
+        }
+
+        string ans = "";
+        for(int i=0; i<v.size(); i++){
+            ans+= (to_string(v[i]) + '#');
+        }
+        return ans;
+    }
+
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
-
+   
         // METHOD 2 ---
         unordered_map<string,vector<string>>m;
         for(int i=0; i<n; i++){
+            // method 2 --
+            /*
             string s = strs[i];
             sort(s.begin(), s.end());
-            if(m.find(s)!=m.end()){
-                m[s].push_back(strs[i]);
-            }
-            else m[s].push_back(strs[i]);
+            */
+            // method 3 -- 
+            string s = getkey(strs[i]);
+
+            m[s].push_back(strs[i]);
         }
         vector<vector<string>>ans;
         for(auto it:m){

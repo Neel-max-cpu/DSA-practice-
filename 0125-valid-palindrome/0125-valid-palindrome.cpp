baf706eab64b05a6d551bbf959d2de77ed1c6291
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-04 10:57:12 +0530

    Time: 8 ms (34.33%), Space: 8.9 MB (54.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        // METHOD 2 -- using alpha numeric - O(n)
+        int i = 0, j= s.size()-1;
+        while(i<j){
+            // isalnum - Alphanumeric characters include letters and numbers, will return true, else return false
+            while(!isalnum(s[i]) && i<j) i++;
+            while(!isalnum(s[j]) && i<j) j--;
+
+            if(tolower(s[i])!=tolower(s[j])) return false;
+            i++;
+            j--;
+        }       
+        return true; 
+
+        // METHOD 1 -- o(n + n)
+        /*
         string t = "";
         for(auto it:s){
             if(it>='a' && it<='z'){
@@ -19,5 +34,6 @@ public:
             if(t[i++]!=t[j--]) return false;
         }
         return true;
+        */
     }
 };

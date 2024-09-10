e3ee295a457d5a4aa502d4a00a635eab040c4436
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-10 21:24:07 +0530

    Time: 10 ms (78.99%), Space: 22.9 MB (72.84%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode*root, int& counter, int k, int &ans){
+        if(root==NULL || counter>=k) return;
+        helper(root->left, counter, k, ans);
+        counter++;
+        if(counter==k) {
+            ans = root->val;
+            return;
+        }
+        helper(root->right, counter, k, ans);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        // brute -- o(n)
+        /*
+        vector<int>ans;
+        // inorder - left root right
+        stack<TreeNode*>s;
+        TreeNode*current = root;        
+        while(!s.empty() || current!=NULL){
+            while(current!=NULL){
+                s.push(current);
+                current=current->left;
+            }
+            current = s.top();
+            s.pop();
+            ans.push_back(current->val);
+            current=current->right;
+        }
+        return ans[k-1];
+        */
+
+        // optimal - using counter
+        int ans= 0;
+        int counter = 0;
+        helper(root,counter,k, ans);
+        return ans;
+    }
+};

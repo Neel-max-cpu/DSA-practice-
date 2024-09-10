2b0cd6b13959c56e92086b63caa19b29cc65c457
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-10 12:35:28 +0530

    Time: 9 ms (35.9%), Space: 15.8 MB (20.23%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -4,6 +4,7 @@ public:
         stack<int>s;    
         int ans = 0;
         for(int i=0; i<tokens.size(); i++){
+            /*
             if(tokens[i]!="+" && tokens[i]!="-" && tokens[i]!="*" && tokens[i]!="/"){
                 s.push(stoi(tokens[i]));
             }
@@ -18,6 +19,23 @@ public:
                 else if(tokens[i]=="/") ans = a/b;
                 s.push(ans);
             }
+            */
+            string t = tokens[i];
+            if(t.size()>1 || isdigit(t[0])){
+                s.push(stoi(t));
+            }
+            else{
+                int b = s.top();
+                s.pop();
+                int a = s.top();
+                s.pop();
+                if(t=="+") ans = a+b;
+                else if(t=="-") ans = a-b;
+                else if(t=="*") ans = a*b;
+                else if(t=="/") ans = a/b;
+                s.push(ans);
+            }
+            
         }
         return s.top();
     }

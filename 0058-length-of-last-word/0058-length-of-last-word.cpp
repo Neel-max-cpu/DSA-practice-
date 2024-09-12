4dc4bc150f962090d6894b22f519edd469619ecc
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-12 23:44:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (5.87%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        // brute --
+        /*
         vector<string>v;
         stringstream ss(s);
         string word;
@@ -9,5 +11,15 @@ public:
 
         string t = v[v.size()-1];
         return t.size();
+        */
+
+        // optimal
+        stringstream ss(s);
+        string word;
+        int last= 0;
+        while(ss>>word){
+            last = word.size();
+        } 
+        return last;
     }
 };

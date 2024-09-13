d3f4caa11ba69f256f3a7fb897cfaaf470d7a13d
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-13 20:18:39 +0530

    Time: 19 ms (77.59%), Space: 29.2 MB (30.6%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -11,10 +11,18 @@
  */
 class BSTIterator {
 public:
+    // method 1 --
+    /*
     vector<int>v;
     int i = 0;
     int size;
+    */
+    
+    // method 2 
+    stack<TreeNode*>s;
     BSTIterator(TreeNode* root) {
+        // method 1
+        /*
         TreeNode *temp = root;
         stack<TreeNode*>s;
         while(temp!=NULL || !s.empty()){
@@ -28,14 +36,46 @@ public:
             temp = temp->right;
         }
         size = v.size();
+        */
+
+        // method 2
+        // put all left, everything on the stack
+        TreeNode*temp = root;
+        while(temp!=NULL){
+            s.push(temp);
+            temp = temp->left;
+        }
     }
     
     int next() {
+        // method 1
+        /*
         return v[i++];
+        */
+        
+        // method 2 - take the top and put all the right into the stack
+        TreeNode *current = s.top();
+        s.pop();
+        int ans = current->val;
+        if(current->right!=NULL) {
+            current=current->right;
+            while(current!=NULL){
+                s.push(current);
+                current=current->left;
+            }
+        }
+        return ans;
     }
     
     bool hasNext() {
+        // method 1
+        /*
         if(i<size) return true;
+        else return false;
+        */
+
+        // method2 
+        if(!s.empty()) return true;
         else return false;
     }
 };

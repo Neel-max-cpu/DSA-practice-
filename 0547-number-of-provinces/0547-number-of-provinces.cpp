0fb24e7c3280c6c5b48f10205b9e1315a332e83c
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-19 13:20:02 +0530

    Time: 14 ms (95.81%), Space: 18.4 MB (83.68%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -9,17 +9,37 @@ public:
         }
     }
 
+    int doFind(vector<int>&parent, int n){
+        int p = parent[n];
+        while(p!=parent[p]){
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+
+    int doUnion(vector<int>&parent, vector<int>&rank, int a, int b){
+        int p1 = doFind(parent, a);
+        int p2 = doFind(parent, b);
+        if(p1 == p2) return 0;
+        if(rank[p1]>rank[p2]){
+            parent[p2] = p1;
+            rank[p1]+=rank[p2];
+        }
+        else{
+            parent[p1] = p2;
+            rank[p2]+=rank[p1];
+        }
+        return 1;
+    }
+
     int findCircleNum(vector<vector<int>>& arr) {
         // similar to connected component in Graph - a2z sheet graph lec1:learning  4th Q
         int n = arr.size();
-        vector<int>parent;
-        vector<int>rank;
-        for(int i=0; i<n; i++){
-            parent.push_back(i);
-            rank.push_back(i);
-        }
-
+  
         // using dfs can be done by bfs too
+        /*
         vector<bool>visited(n, false);
         int count = 0;
         for(int i=0; i<n; i++){
@@ -29,5 +49,30 @@ public:
             }
         }
         return count;
+        */
+
+        // using union find --
+        vector<int>parent;
+        vector<int>rank;
+        for(int i=0; i<n; i++){
+            parent.push_back(i);
+            rank.push_back(i);
+        }   
+
+        int result = n;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                // 0 means not connected and a node is itself 
+                if(i==j || arr[i][j]==0) continue;
+                else if(arr[i][j]==1){
+                    int a = i;
+                    int b = j;
+                    result -= doUnion(parent, rank, a, b);
+                }
+            }
+        }
+        return result;
+
+
     }
 };

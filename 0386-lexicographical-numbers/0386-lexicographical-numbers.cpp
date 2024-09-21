5a5a54b4000cbcda9246879907e2e1a8ca001884
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-09-22 00:16:38 +0530

    Time: 4 ms (88.45%), Space: 12.5 MB (91.06%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -13,10 +13,39 @@ public:
     }
     vector<int> lexicalOrder(int n) {
         vector<int>ans;       
+        // brute - 
+        /*
+        vector<string>v;
+        for(int i=1; i<=n; i++){
+            v.push_back(to_string(i));
+        }
+        sort(v.begin(), v.end());
+        for(auto it:v){
+            ans.push_back(stoi(it));
+        }
+        */
+
+        // better - dfs
+        /*
         for(char c = '1'; c<='9'; c++){
             // c string of len 1
             string s(1,c);
             helper(n, s, ans);
+        }        
+        return ans;
+        */
+
+        // iterative --
+        int curr = 1;
+        for(int i=1; i<=n; i++){
+            ans.push_back(curr);
+            if(curr*10<=n){
+                curr = curr*10;
+            }
+            else{
+                while(curr%10 ==9 || curr>=n) curr = curr/10;
+                curr+=1;
+            }
         }
         return ans;
     }

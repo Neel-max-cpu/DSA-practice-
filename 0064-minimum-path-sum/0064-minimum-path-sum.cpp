662a345d7f48231499fd7099761bf7157a3e94b7
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-10-03 23:29:50 +0530

    Time: 4 ms (90.23%), Space: 14.9 MB (16.94%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -20,6 +20,28 @@ public:
         return dp[row][col] = min(up,left);
     }
 
+    int tab(int m, int n, vector<vector<int>>&arr){
+        vector<vector<int>>dp(m, vector<int>(n,0));
+
+        dp[0][0] = arr[0][0];
+        for(int i=1; i<m; i++){        
+            dp[i][0] = arr[i][0]+dp[i-1][0];
+        }
+
+        for(int i=1; i<n; i++){
+            dp[0][i] = arr[0][i] + dp[0][i-1];
+        }
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                int ans = arr[i][j];
+                ans += min(dp[i-1][j], dp[i][j-1]);
+                dp[i][j] = ans;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -27,9 +49,14 @@ public:
         // recrusion ---
         // return rec(m, n, m-1, n-1, grid);
 
-        // dp memoization 
+        // dp memoization ---
+        /*
         vector<vector<int>>dp(m, vector<int>(n, -1));
         return memo(m, n, m-1, n-1, grid, dp);
+        */
+
+        // dp tabulation --
+        return tab(m, n, grid);
         
     }
 };

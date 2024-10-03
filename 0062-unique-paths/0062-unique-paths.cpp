5ee76697a4c21023cad60fb3081b244f6235d5b0
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-10-03 22:44:07 +0530

    Time: 0 ms (100%), Space: 7.7 MB (70.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,6 +19,37 @@ public:
         if(row+1<m) down += memo(m, n, row+1, col, dp);
         return dp[row][col] = right+down; 
     }
+    
+    int memo2(int m, int n, int row, int col, vector<vector<int>>&dp){
+        if(row==0 && col==0) return 1;
+
+        if(dp[row][col]!=-1) return dp[row][col];
+
+        if(row<0 || col<0) return 0;
+        int up = memo2(m, n, row-1, col, dp);
+        int left = memo2(m, n, row, col-1, dp);
+        return dp[row][col] = up+left; 
+    }
+
+    int so(int m, int n){
+        vector<int>prev(n, 0);        
+        for(int i=0; i<m; i++){
+            vector<int>temp(n,0);
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) {
+                    temp[j] = 1;
+                    continue;
+                }
+                int up=0, left = 0;
+                if(i>0) up = prev[j];
+                if(j>0) left = temp[j-1];
+                temp[j] = up+left;
+            }            
+            prev = temp;
+        }
+        return prev[n-1];
+
+    }
 
     int uniquePaths(int m, int n) {
         // recursion --
@@ -27,10 +58,12 @@ public:
         // dp memoization ---
         /*
         vector<vector<int>>dp(m, vector<int>(n,-1));
-        return memo(m, n, 0, 0, dp);
+        // return memo(m, n, 0, 0, dp);
+        return memo2(m, n, m-1, n-1, dp);
         */
 
         // dp tabulation ---
+        /*
         vector<vector<int>>dp(m, vector<int>(n,0));
         // base case
         for(int i=0; i<n; i++){
@@ -46,6 +79,9 @@ public:
             }
         }
         return dp[m-1][n-1];
-        
+        */
+
+        // space op
+        return so(m, n);
     }
 };

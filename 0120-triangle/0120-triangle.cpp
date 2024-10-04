e85212026f0ce66d0c45fc38c9066f549b310acd
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-10-04 22:29:36 +0530

    Time: 4 ms (74.59%), Space: 11.5 MB (74.77%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -19,13 +19,59 @@ public:
         return dp[row][col] = min(up, down);
     }
 
+    int tab(vector<vector<int>>&arr, int m){
+        vector<vector<int>>dp(m, vector<int>(m, 0));
+        for(int i=0;i<arr[m-1].size(); i++){
+            dp[m-1][i] = arr[m-1][i];
+        }
+
+        for(int i=m-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                dp[i][j] = arr[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+
+        // for(auto it:dp) {
+        //     for(auto x:it)
+        //         cout<<x<<" ";
+        //     cout<<endl;
+        // }
+        return dp[0][0];
+    }
+    
+    int so(vector<vector<int>>&arr, int m){
+        vector<int>next(m, 0);
+        for(int i=0;i<arr[m-1].size(); i++){
+            next[i] = arr[m-1][i];
+        }
+
+        for(int i=m-2; i>=0; i--){
+            vector<int>temp(m, 0);
+            for(int j=i; j>=0; j--){
+                temp[j] = arr[i][j] + min(next[j], next[j+1]);
+            }
+            next = temp;
+        }
+        return next[0];
+
+       
+    }
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         // recursion ---
         // return rec(0,0, triangle);
 
         // dp memoization--
+        /*
         vector<vector<int>>dp(m, vector<int>(m, -1));
         return memo(0,0, triangle, dp);
+        */
+
+        // dp tabulation ---
+        // return tab(triangle, m);
+
+        // sp optimization
+        return so(triangle, m);
     }
 };

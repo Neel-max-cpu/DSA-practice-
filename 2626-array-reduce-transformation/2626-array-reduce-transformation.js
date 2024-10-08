ffc71d0c689773099c61696902aeac9a3f469116
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-10-08 23:30:11 +0530

    Time: 55 ms (49.48%), Space: 49.1 MB (64.41%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let n = nums.length;
+    if(n===0) return init;
+    let val = init;
+    for(let i=0; i<n; i++){
+        val = fn(val, nums[i]);
+    }
+    return val;
+};

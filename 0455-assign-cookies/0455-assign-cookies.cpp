520a49e79e8e58417d342055284978982e2266b1
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-10-15 13:02:10 +0530

    Time: 45 ms (38.97%), Space: 44.8 MB (60.13%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        // brute ---
+        /*
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());        
+        int count = 0;
+        for(int i=0; i<g.size(); i++){
+            for(int j=0; j<s.size(); j++){
+                if(s[j]==-1) continue;
+                if(s[j]>=g[i]) {
+                    count++;
+                    s[j] = -1;
+                    break;
+                }
+            }
+        }
+        return count;
+        */
+
+        // better ---
+        if(g.size()==0 || s.size()==0) return 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int count = 0;
+        int i = 0, j = 0;        
+        if(g[i]>s[s.size()-1]) return 0;
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i]){
+                count++;
+                j++;
+                i++;
+            }
+            else if(s[j]<g[i]) j++;
+        }
+        return count;
+
+
+
+    }
+};

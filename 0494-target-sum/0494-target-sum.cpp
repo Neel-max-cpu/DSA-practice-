4688e9edbd6c38501d04ec136236553d8e2903e0
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-10-21 13:17:11 +0530

    Time: 432 ms (26.18%), Space: 89.7 MB (6.53%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -12,12 +12,30 @@ public:
 
         return pos+neg;
     }
+    
+    int memo(vector<int>&arr, int k, int sum, int n, int i, unordered_map<string,int>&dp){
+        if(i<0){
+            if(k==sum) return 1;
+            else return 0;
+        }
+        string key = to_string(i)+","+to_string(sum);
+
+        if(dp.find(key)!=dp.end()) return dp[key];
+
+        int pos = 0, neg = 0;
+        pos = memo(arr, k, sum+arr[i], n, i-1, dp);
+        neg = memo(arr, k, sum-arr[i], n, i-1, dp);
+    
+        return dp[key] = pos+neg;
+    }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         //recursion ---
         int n = nums.size();
-        return rec(nums, target, 0, n, n-1);
+        // return rec(nums, target, 0, n, n-1);
 
         // dp memoization ---
+        unordered_map<string,int>dp;
+        return memo(nums, target, 0, n, n-1, dp);
     }
 };

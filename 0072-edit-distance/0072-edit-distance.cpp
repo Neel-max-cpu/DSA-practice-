2977648f43266bc5a7ec9c5f1304612d2849f94e
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-10-29 08:59:45 +0530

    Time: 6 ms (64.2%), Space: 13.5 MB (20.48%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -51,6 +51,29 @@ public:
 
     }
 
+    int tab(string s1, string s2, int n, int m){
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+
+        // base 
+        dp[0][0] = 0;
+        for(int j=1; j<=m; j++) dp[0][j] = j;
+        for(int i=1; i<=n; i++) dp[i][0] = i;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                int one = 0;
+                if(s1[i-1]==s2[j-1]) one = dp[i-1][j-1];
+                else one = 1+ dp[i-1][j-1];
+                int two = 1, three=1;
+                two += dp[i-1][j];
+                three += dp[i][j-1];
+
+                dp[i][j] = min({one, two, three});
+            }
+        }
+        return dp[n][m];
+    }
+
     int minDistance(string word1, string word2) {
         // recrusion ---
         int n = word1.size();
@@ -61,7 +84,12 @@ public:
         // return rec(word1, word2, n-1, m-1);
 
         // dp memoization ---
+        /*
         vector<vector<int>>dp(n, vector<int>(m, -1));
         return memo(word1, word2, n-1, m-1, dp);
+        */
+
+        // dp tabulation --
+        return tab(word1, word2, n, m);
     }
 };

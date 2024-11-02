80dd56af40a265eaec931e781bcd37803e6ba4aa
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-11-02 22:36:06 +0530

    Time: 23 ms (74.23%), Space: 12.5 MB (74.91%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -24,10 +24,35 @@ public:
         return dp[i][j] = ans;
     }
 
+    int tab(vector<int>&arr, int c){
+        // n+2 for safety
+        vector<vector<int>>dp(c+2, vector<int>(c+2, 0));
+        
+        for(int i=c; i>=1; i--){
+            for(int j=1; j<=c; j++){
+                // base case -- don't need initiallised with 0 
+                // if(i>j) dp[i][j] = 0;
+                if(i>j) continue;
+                // else{
+                    int ans = 1e9;
+                    for(int k=i; k<=j; k++){             
+                        int size = (arr[j+1] - arr[i-1]) + dp[i][k-1] + dp[k+1][j];
+                        ans = min(size, ans);
+                    }
+                    dp[i][j] = ans;
+                // }
+            }
+        }
+        return dp[1][c];
+    }
+
     int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(), cuts.end());
+        
+        
+        /*
         int size = cuts.size();
         // important sort it---
-        sort(cuts.begin(), cuts.end());
         vector<int>v;
         v.push_back(0);
         for(int i=0; i<size; i++){
@@ -37,12 +62,25 @@ public:
         int len = v.size();
         // start from 1 and len-2 , since that's the size of the cuts arry 0 and n are added to the front
         // and the end buy us
+        */
+
+        int c = cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(), 0);
 
         // recursion ---
         // return rec(v, 1, len-2);
+        // return rec(cuts, 1, c);
 
         // dp memoization --
-        vector<vector<int>>dp(len, vector<int>(len, -1));
-        return memo(v, 1, len-2, dp);
+        /*
+        // vector<vector<int>>dp(len, vector<int>(len, -1));
+        // return memo(v, 1, len-2, dp);
+        vector<vector<int>>dp(c+1, vector<int>(c+1, -1));
+        return memo(cuts, 1, c, dp);
+        */
+
+        // dp tabulation ---
+        return tab(cuts, c);
     }
 };

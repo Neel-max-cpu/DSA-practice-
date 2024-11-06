458dc2af0a3834b14dea07af5febe09035f94dbc
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-11-06 11:36:48 +0530

    Time: 312 ms (34.77%), Space: 88.7 MB (56.72%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let map = new Map();
+    return function(...args) {
+        let key = JSON.stringify(args);
+        if(map.has(key)) return map.get(key);
+        else {
+            let a = fn(...args);
+            map.set(key, a);
+            return a;    
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */

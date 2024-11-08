285680bff7caa8a762c392c53b633eaeab872f24
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-11-08 11:02:36 +0530

    Time: 4 ms (56.63%), Space: 9.8 MB (61.74%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -11,7 +11,21 @@ public:
             else if(c==')'){
                 char gate = gates.top();
                 gates.pop();
+                /*
+                For an AND operation, all values within the parentheses need to be true 
+                for the entire expression to be true.Therefore, we start with result = true. 
+                This way, any false within the expression will change result to false as soon as 
+                it is encountered
 
+                For an OR operation, the expression is true if at least one value 
+                inside the parentheses is true.Starting with result = false allows us to change 
+                result to true upon encountering the first true value.
+
+                Although result is initially set to true, this is just a placeholder. The actual 
+                result will be the opposite of the first operand within the parentheses, as processed 
+                in result = !boolVal.
+
+                */
                 bool res;
                 if(gate=='&' || gate=='!') res = true;
                 else res = false;

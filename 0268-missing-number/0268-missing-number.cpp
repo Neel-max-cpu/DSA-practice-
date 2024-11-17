e4436da3427934a50c30e58355221902f0fa1143
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-11-17 11:54:29 +0530

    Time: 0 ms (100%), Space: 21.2 MB (24.06%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,10 +2,23 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
+        /*
         sort(nums.begin(), nums.end());
         for(int i=0; i<n; i++){
             if(nums[i]!=i) return i;
         }
         return n;
+        */
+
+        vector<int>v(n+1,-1);
+        for(int i=0; i<n; i++){
+            v[nums[i]] = 1;
+        }
+
+        for(int i=0; i<n+1; i++){
+            if(v[i]==-1) return i;
+        }
+        // temp return
+        return -1;
     }
 };

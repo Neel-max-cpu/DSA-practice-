33f639fe916815b4e5c9134bd6ff118948f6759c
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-11-17 12:00:32 +0530

    Time: 0 ms (100%), Space: 20.7 MB (75.62%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -10,6 +10,8 @@ public:
         return n;
         */
 
+
+        /*
         vector<int>v(n+1,-1);
         for(int i=0; i<n; i++){
             v[nums[i]] = 1;
@@ -20,5 +22,31 @@ public:
         }
         // temp return
         return -1;
+        */
+
+
+        // optimial ---
+        /*
+        int sum = n*(n+1)/2;
+        int count = 0;
+        for(auto it:nums){
+            count+=it;
+        }
+        return sum-count;
+        */
+
+
+        // same using xor a^a = 0, so 
+        // so xor1 is the xor from 0 to n
+        // xor2 should be the xor of all the ele form the array
+        // so xor1 ^ xor2 would give us the missing number
+
+        int xor1 = 0, xor2 = 0;
+        for(int i=0; i<n; i++){
+            xor1^=i;
+            xor2^=nums[i];
+        }
+        xor1^=n;
+        return xor1^xor2;
     }
 };

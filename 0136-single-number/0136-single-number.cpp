ac4c34c1d0f42c13bab7f5971b6ca828253f7c91
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2024-11-18 09:34:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (97.4%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+
+        /*
         unordered_map<int,int>m;
         for(auto it:nums){
             m[it]++;
@@ -9,5 +11,12 @@ public:
             if(it.second==1) return it.first;
         }
         return -1;
+        */
+
+        int ele = 0;
+        for(auto it:nums){
+            ele^=it;
+        }
+        return ele;
     }
 };

b9891ddce5bc58fa0e7bc29bcf16babfb1bbabc9
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2025-01-28 11:50:45 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.97%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -5,6 +5,8 @@ public:
         int m = s.size();
         if(n!=m) return false;
 
+        // brute force ---
+        /*
         if(s==goal) return true;
         int i=1;
         string t = "";
@@ -16,6 +18,35 @@ public:
             i++;
         }   
         return false;
+        */
+
+        // brute force ---
+        /*
+        for(int i=0; i<n; i++){
+            rotate(s.begin(), s.begin()+1, s.end());
+            if(s==goal) return true;
+        }
+        return false;
+        */
+
+        // BETTER method -- o(n)
+        // /*
+        string doublestring = s+s;
+
+        // .find in substring returs the first index when the substring is found
+        // int x = doublestring.find(goal);
+        // if(x!=string::npos) {
+        //     cout<<"pos: "<<x<<endl;
+        //     return true;
+        // }
+        // else return false;
+
+        // OR -- since .find returns the pos so, if found then it should be < length of the double string
+        return doublestring.find(goal) < doublestring.size();
+        // */
+
+
+        // best -- kmp algo o(n) but not need
 
     }
 };

77d1a13a1fcf5d0fbfb4e6599abc60ce9d5275a6
Neel Bhattacharya <85828440+Neel-max-cpu@users.noreply.github.com>
2025-02-06 10:55:55 +0530

    Time: 59 ms (11.66%), Space: 75.7 MB (22.24%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,8 +10,50 @@
  */
 class Solution {
 public:
+
+    ListNode *findmiddle(ListNode *head){
+        if(head==NULL || head->next == NULL) return head;
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    ListNode *merge(ListNode *left, ListNode*right){
+        ListNode *newhead = new ListNode(-1);
+        ListNode *temp = newhead;
+        while(left && right){
+            if(left->val > right->val){
+                temp->next = right;
+                right = right->next;
+            }
+            else{
+                temp->next = left;
+                left = left->next;
+            }
+            temp = temp->next;
+        }
+
+        if(left!=NULL){
+            temp->next = left;
+        }
+        if(right!=NULL){
+            temp->next = right;
+        }
+
+        return newhead->next;
+
+    }
+
     ListNode* sortList(ListNode* head) {
         if(head==NULL || head->next == NULL) return head;
+
+        // brute ---
+        /*
         vector<int>v;
         ListNode *temp = head;
         while(temp){
@@ -27,5 +69,19 @@ public:
             temp = temp->next;
         }
         return newhead;
+        */
+        
+        // best --- mergesort
+        ListNode *middle = findmiddle(head);
+        ListNode *right = middle->next;
+        middle->next = NULL;
+        ListNode *left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left, right);
+
     }
+    
 };

58818ec64a9754062f3f0059d55303a1186ffa05
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-09-09 23:27:00 +0530

    Time: 0 ms (100%), Space: 10.2 MB (35.34%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,81 +1,21 @@
-#include<vector>
-
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        /*
-        vector<vector<int>>ans;
-        for(int j=0; j<n; j++){
-            vector<int>temp;
-            for(int i=n-1; i>=0; i--){
-                temp.push_back(matrix[i][j]);
-            }
-            ans.push_back(temp);
-        }
-        */
-        // another way both brute force
-        /*
-        vector<vector<int>>ans(n, vector<int>(n,0));
+    void rotate(vector<vector<int>>& arr) {
+        int n = arr.size();
+
+        //transpose in place 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                int k = (n-1)-i;
-                ans[j][k] = matrix[i][j];
+                if(i==j) continue;
+                else if(i<j){
+                    swap(arr[i][j], arr[j][i]);
+                }
             }
-        }
+        }       
 
-        for(int i = 0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                matrix[i][j] = ans[i][j];
-            }
-        }
-        */
-
-        // optimal approach -- transpose the matrix and then reverse each row
-
-        // transpose ---         
-        /*
-        [a b c]   ->  [a d]
-        [d e f]       [b e]
-                      [c f]
-
-        eg1 
-        [1,2,3] -> [1,4,7]
-        [4,5,6]    [2,5,8]
-        [7,8,9]    [3,6,9]
-
-        reverse each row
-
-        [1,4,7] ->  [7,4,1]
-        [2,5,8]     [8,5,2]
-        [3,6,9]     [9,6,3]
-                        
-        
-        
-        */
-        // o(n*n)
-        // transpose
-        /*
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-        */
-
-        // better way 
+        //reverse each row
         for(int i=0; i<n; i++){
-            for(int j=0; j<i;j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
+            reverse(arr[i].begin(), arr[i].end());
         }
-
-        // reverse each row
-        //o(n*n)
-        for(int i=0; i<n; i++){
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-
-        
     }
 };

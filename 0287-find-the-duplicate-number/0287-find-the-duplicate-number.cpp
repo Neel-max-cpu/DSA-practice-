61ef46bb64f74e2e40c012c805a4597d2afc1dcd
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-09-16 00:00:13 +0530

    Time: 8 ms (49.19%), Space: 64.9 MB (89.99%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,49 +1,29 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        int n = nums.size();
+    int findDuplicate(vector<int>& arr) {
+        // int i = 0;
+        // int curr = -1;
+        // while(true){
+        //     if(arr[i]==-1) return curr;
+        //     curr = arr[i];            
+        //     arr[i] = -1;
+        //     i = curr;
+        // }
+        // return -1;
 
-        int slow = nums[0];
-        int fast = nums[0];
+        int fast = arr[0];
+        int slow = arr[0];
         do{
-            // move the slow pointer by 1 and fast pointer by 2
-            slow = nums[slow];
-            fast = nums[nums[fast]];
+            fast = arr[arr[fast]];
+            slow = arr[slow];
         }
         while(slow!=fast);
 
-        fast = nums[0];
-        while(slow!=fast){
-            slow = nums[slow];
-            fast = nums[fast];
+        int newPointer = arr[0];
+        while(slow!=newPointer){
+            slow = arr[slow];
+            newPointer = arr[newPointer];
         }
         return slow;
-
-
-
-        /*
-        vector<int>v(n+1, 0);
-        // for(int i=0; i<n+1; i++){
-        //     v[i] = i;
-        // }
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            int x = nums[i];
-            // if(v[x] == x){
-            //     v[x] = 0;
-            // }
-            // else{
-            //     ans = x;
-            //     break;
-            // }
-            if(v[x] ==0 ) v[x] = 1;
-            else if(v[x]==1){
-                ans = x;
-                break;
-            }
-        }
-        return ans;
-        */
-        
     }
 };

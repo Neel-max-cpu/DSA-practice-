70a133c11f8623289597e56e3ebb8091d5020f3a
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-09-17 23:22:30 +0530

    Time: 4 ms (1.96%), Space: 8.4 MB (99.64%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,11 @@
+#include<cmath>
+#include<algorithm>
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 0.000000;
+        ans = pow(x,n);
+        return ans;
+    }
+};

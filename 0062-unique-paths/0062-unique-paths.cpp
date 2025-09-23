6c18fa0675b5fd3b05b78e00f9d785b6d59a5a1e
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-09-23 08:01:06 +0530

    Time: 1 ms (24.74%), Space: 9.2 MB (58.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -43,9 +43,23 @@ public:
         // int count = doPathCheck(m, n, 0,0);
 
         //step 1
-        vector<vector<int>>dp(m+1, vector<int>(n+1,-1)); 
-        dp[0][0] = 1;       
+        /*
+        vector<vector<int>>dp(m+1, vector<int>(n+1,-1));             
         int count = doPathCheck2(m, n, m-1, n-1, dp);
         return count;
+        */
+
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        dp[0][0] = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) continue;
+                int one=0, two=0;
+                if(i-1>=0) one = dp[i-1][j];
+                if(j-1>=0) two = dp[i][j-1];
+                dp[i][j]= one+two;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };

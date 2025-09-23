c6530f0adef0e341888ff0b986c571bba0fdfe91
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-09-23 07:44:17 +0530

    Time: 0 ms (100%), Space: 9.5 MB (8.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,87 +1,51 @@
 class Solution {
 public:
-    int rec(int m, int n, int row, int col){
-        if(row==m-1 && col==n-1) return 1;
-
-        int right = 0, down = 0;
-        if(col+1<n) right += rec(m, n, row, col+1);
-        if(row+1<m) down += rec(m, n, row+1, col);
-        return right+down; 
+    bool valid(int m, int n, int i, int j){
+        if(i==m || j==n) return false;
+        return true;
     }
-    
-    int memo(int m, int n, int row, int col, vector<vector<int>>&dp){
-        if(row==m-1 && col==n-1) return 1;
-
-        if(dp[row][col]!=-1) return dp[row][col];
-
-        int right = 0, down = 0;
-        if(col+1<n) right += memo(m, n, row, col+1, dp);
-        if(row+1<m) down += memo(m, n, row+1, col, dp);
-        return dp[row][col] = right+down; 
+    bool valid2(int m, int n, int i, int j){
+        if(i<0 || j<0) return false;
+        return true;
     }
-    
-    int memo2(int m, int n, int row, int col, vector<vector<int>>&dp){
-        if(row==0 && col==0) return 1;
-
-        if(dp[row][col]!=-1) return dp[row][col];
-
-        if(row<0 || col<0) return 0;
-        int up = memo2(m, n, row-1, col, dp);
-        int left = memo2(m, n, row, col-1, dp);
-        return dp[row][col] = up+left; 
-    }
-
-    int so(int m, int n){
-        vector<int>prev(n, 0);        
-        for(int i=0; i<m; i++){
-            vector<int>temp(n,0);
-            for(int j=0; j<n; j++){
-                if(i==0 && j==0) {
-                    temp[j] = 1;
-                    continue;
-                }
-                int up=0, left = 0;
-                if(i>0) up = prev[j];
-                if(j>0) left = temp[j-1];
-                temp[j] = up+left;
-            }            
-            prev = temp;
+    int doPathCheck(int m, int n, int starti, int startj){
+        if(starti==m-1 && startj==n-1){
+            return 1;
+        }        
+        int count1 = 0, count2=0;
+        if(valid(m,n, starti+1, startj)){            
+            count1 = doPathCheck(m, n, starti+1, startj);
         }
-        return prev[n-1];
+        if(valid(m,n, starti, startj+1)){
+            count2 = doPathCheck(m, n, starti, startj+1);
+        }
+        return count1+count2;
+    }    
+    int doPathCheck2(int m, int n, int starti, int startj, vector<vector<int>>&dp){
+        if(starti==0 && startj==0){
+            return 1;
+        }           
 
+        //step3
+        if(dp[starti][startj]!=-1) return dp[starti][startj];
+
+        int count1 = 0, count2=0;
+        if(valid2(m,n, starti-1, startj)){            
+            count1 = doPathCheck2(m, n, starti-1, startj, dp);
+        }
+        if(valid2(m,n, starti, startj-1)){
+            count2 = doPathCheck2(m, n, starti, startj-1, dp);
+        }
+        //step2
+        return dp[starti][startj] = count1+count2;
     }
-
     int uniquePaths(int m, int n) {
-        // recursion --
-        // return rec(m, n , 0, 0);
-        
-        // dp memoization ---
-        /*
-        vector<vector<int>>dp(m, vector<int>(n,-1));
-        // return memo(m, n, 0, 0, dp);
-        return memo2(m, n, m-1, n-1, dp);
-        */
+        // int count = doPathCheck(m, n, 0,0);
 
-        // dp tabulation ---
-        /*
-        vector<vector<int>>dp(m, vector<int>(n,0));
-        // base case
-        for(int i=0; i<n; i++){
-            dp[0][i] = 1;
-        }
-        for(int i=1; i<m; i++){
-            dp[i][0] = 1;
-        }
-
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
-        */
-
-        // space op
-        return so(m, n);
+        //step 1
+        vector<vector<int>>dp(m+1, vector<int>(n+1,-1)); 
+        dp[0][0] = 1;       
+        int count = doPathCheck2(m, n, m-1, n-1, dp);
+        return count;
     }
 };

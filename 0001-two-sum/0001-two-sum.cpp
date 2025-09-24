c28245895cebd910c59ac48dd5982bc55e26d830
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-09-24 22:37:37 +0530

    Time: 0 ms (100%), Space: 14.8 MB (36.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,127 +1,23 @@
-#include<unordered_map>
-
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-
-        
-        int n = nums.size();
-        vector<int>ans;
+    vector<int> twoSum(vector<int>& arr, int target) {
+        //brute - o(n^2) - two loops
+        // optimize o(nlogn) - sort then 2 pointers
+        // best hashmap?
+        int n = arr.size();
         unordered_map<int,int>m;
-        for(int i=0; i<n; i++){
-            int x = target - nums[i];
-            if(m.find(x)!=m.end()){
-                ans.push_back(m[x]);
-                ans.push_back(i);
-                break;
+        for(int i = 0; i<n; i++){
+            int x = arr[i];
+            int left = target-x;
+            if(m.find(left)!=m.end()){
+                //if found
+                return {i, m[left]};
             }
-            m[nums[i]] = i;
-        }
-        return ans;
-
-
-
-
-        /*
-        vector<int>ans;
-        unordered_map<int,int>m;
-        for(int i=0; i<nums.size(); i++){
-            m[nums[i]]= i;
-        }
-        
-        for(int i=0; i<nums.size(); i++){
-            int temp = target - nums[i];
-            if(m[temp]!=0){
-                if(m[temp] == i) continue;
-                else{
-                    ans.push_back(i);
-                    ans.push_back(m[temp]);
-                    return ans;
-                }
+            else{
+                //put the element in the map
+                m[x] = i;
             }
         }
-        return ans;
-        */
-
-
-        /*
-        for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i]+nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
-                    
-            }
-        }
-        return ans;
-        */
-
-        /*
-        unordered_map<int,int>m;
-        for(int i=0; i<nums.size(); i++)
-            m[nums[i]] = i;
-        
-       for(int i=0; i<nums.size(); i++){
-            int temp = target - nums[i];
-            if(m[temp]!=0){
-                if(m[temp] == i) continue;
-                else{
-                    ans.push_back(i);
-                    ans.push_back(m[temp]);
-                    break;
-                }
-            }
-        }
-        return ans;
-        */
-
-        /*
-        unordered_map<int, int> mpp;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int moreNeeded = target - num;
-            if (mpp.find(moreNeeded) != mpp.end()) {
-                return {mpp[moreNeeded], i};
-            }
-            mpp[num] = i;
-        }
-        return { -1, -1};
-        */
-        
-        // 2pointer not working
-        /*
-        int i =0, j = nums.size()-1;
-        while(i<=j){
-            int x = nums[i] + nums[j];
-            // cout<<x<<" "<<i<<" "<<j<<endl;
-            if(x == target){
-                // return {i, j};
-                break;
-            }
-            else if(x>target){
-                if(target>=0)
-                    j--;
-                else{
-                    i++;
-                }
-            }
-            else if(x<target) {
-
-                if(target>=0)
-                    i++;
-                else{
-                    j--;
-                }
-            }
-        }
-        return {i,j};
-        // return {-1,-1};
-        */
-        
-       
-       
+        return{-1,-1};
     }
 };

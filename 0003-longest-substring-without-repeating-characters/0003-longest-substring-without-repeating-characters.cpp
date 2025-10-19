56be4e02118df7ae5e93022ba5df68a8cc28ed82
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-10-19 20:00:59 +0530

    Time: 10 ms (51.82%), Space: 11.2 MB (90.61%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,72 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int start = 0, end = 0;
-        unordered_map<char,int>m;
-        int len = 0;
-        while(end<n){
-            char x = s[end];
-            if(m.find(x)!=m.end()){
-                //if found
-                len = max(len, end-start);
-                start = end;
-                m[x] = end;
-                end++;
+    int lengthOfLongestSubstring(string input) {
+        int n = input.size();
+        /*
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            set<char>s;
+            int count = 0;
+            for(int j=i; j<n; j++){
+                // if not found
+                if(s.find(input[j]) == s.end()){
+                    s.insert(input[j]);
+                    count++;
+                }
+                // if found
+                else{
+                    break;
+                }
             }
-            else{
-                //not found
-                m[x] = end;
-                end++;
-            }
+            ans = max(ans, count);
         }
-        return max(end-start, len);
+        return ans;
+        */
+
+        // better ---- 
+
+        /*
+        int ans =0;
+        int left = 0, right = 0;
+        set<char>s;
+        for(int right = 0; right<n; right++){
+            if(s.find(input[right])!=s.end()){
+                // if found
+                while(left<right && s.find(input[right])!=s.end()){
+                    s.erase(input[left]);
+                    left++;
+                }
+            }
+            // not found
+            ans = max(ans, right-left+1);
+            s.insert(input[right]);
+        }
+        return ans;
+        */
+
+            //    best -- 
+    // /*
+
+
+        vector<int>v(256, -1);
+        // vector<char>v(256, -1);
+        // map<char,int>v;
+        int left = 0;
+        int ans = 0;
+        int right = 0;
+        while(right<n){
+            if(v[input[right]] != -1){
+            // if(v.find(input[right]) != v.end()){
+                // alread present
+                // take the right + 1 since we have to update the left to that place
+                left = max(v[input[right]] + 1, left);
+            }
+            // if not found/even if found update the index
+            v[input[right]] = right;
+            ans = max(ans, right-left+1);
+            right++;
+        }
+        return ans;
+        // */
     }
 };

d95aee245606f68e22ac7c8ec4b7b6f4f7104b7a
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-10-19 20:23:52 +0530

    Time: 7 ms (64.99%), Space: 12 MB (46.7%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,6 +5,8 @@ public:
         int l = 0, r=0;
         int len = 0;
         unordered_set<char>st;
+
+        //better approach o(2n worst case l also have to traverse the entire array with r so n+n)
         /*
         while(r<n){
             char x = s[r];
@@ -30,7 +32,8 @@ public:
             }
         }
         */
-
+        //better --- o(2n)
+        /*
         while(r<n){
             char x = s[r];
 
@@ -40,6 +43,36 @@ public:
                 l++;
             }
             st.insert(x);
+            len = max(len, r-l+1);
+            r++;
+        }
+        */
+
+        //best o(n) - instead of traversing the l to r why not directly jump l to the req postion
+        // so instead of taking a set we take a map with char and the last index it was found
+        unordered_map<char,int>m;
+        while(r<n){
+            char x = s[r];
+            //if duplicate found
+            if(m.find(x)!=m.end()){
+                // just go the next index of the last one -- also edge case l must always move forward
+                /*
+                s = "abba"
+                r=0 → 'a' → m[a]=0, len=1
+                r=1 → 'b' → m[b]=1, len=2
+                r=2 → 'b' found → l jumps to 2 (m[b]+1)
+                r=3 → 'a' found → l jumps to 1 (m[a]+1) ❌ but that’s backward!
+                */
+                // if last seen is less than l then no need to worry l is at correct pos
+                int nextIndex = max(l,m[x]+1);  
+                l = nextIndex;
+                // we could erase the element from the map 
+                // m.erase(x); -- but no need as its getting updated below,
+                // and len is alos being calculated and r++ is being done
+            }
+
+            //not found put the value and index
+            m[x] = r;
             len = max(len, r-l+1);
             r++;
         }

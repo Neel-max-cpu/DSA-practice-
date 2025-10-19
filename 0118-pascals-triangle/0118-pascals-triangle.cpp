8392d90cc35094be0a3af614fc19c8c9421ec8fb
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-10-19 23:01:16 +0530

    Time: 0 ms (100%), Space: 9.9 MB (10.16%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
-
-    vector<int>generateRow(int row){
-        int ans = 1;
-        vector<int>v;
-        v.push_back(1);
-        for(int i=1; i<row; i++){
-            ans = ans*(row-i);
-            ans = ans/i;
-            v.push_back(ans);
-        }
-        return v;
-    }
-    vector<int>generate(vector<int>&temp, int row){
-        vector<int>ans(row);
-        ans[0] = 1;
-        for(int i=1; i<row-1; i++){
-            ans[i] = temp[i-1]+temp[i];
-        }
-        ans[row-1] = 1;
-        return ans;
-    }
-
     vector<vector<int>> generate(int n) {
-        /*
-        vector<vector<int>>v1;
-        for(int i=1; i<=n; i++){
-            vector<int>temp = generateRow(i);
-            v1.push_back(temp);
+        vector<vector<int>>ans;
+        ans.push_back({1});
+        if(n==1){
+            return ans;
         }
-        return v1;
-        */
-        if(n==1){            
-            return {{1}};
+        ans.push_back({1,1});
+        if(n==2){
+            return ans;
         }
-        else{
-            vector<vector<int>>v;
-            v.push_back({1});
-            v.push_back({1,1});
-            for(int i=2; i<n; i++){
-                vector<int>temp=v[i-1];
-                vector<int>arr = generate(temp, i+1);
-                v.push_back(arr);
+        n-=2;
+        while(n>0){
+            vector<int>temp;
+            temp.push_back(1);
+            vector<int>temp2 = ans.back();
+            for(int i=0;i<temp2.size()-1; i++){
+                int val = temp2[i]+temp2[i+1];
+                temp.push_back(val);
             }
-            return v;            
+            temp.push_back(1);
+            ans.push_back(temp);
+            n--;
         }
+        return ans;
     }
 };

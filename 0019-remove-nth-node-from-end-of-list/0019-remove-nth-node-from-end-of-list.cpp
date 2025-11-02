99123ca5172f920724ae36c0f949928d8d6c8b5a
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-11-02 22:25:07 +0530

    Time: 0 ms (100%), Space: 15 MB (25.28%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,71 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head==NULL) return NULL;
-
-        /*
+    int countLen(ListNode*head){
         int count = 0;
-        ListNode *temp = head;
-        while(temp!=NULL){
-            temp=temp->next;
+        while(head){
             count++;
+            head = head->next;
         }
+        return count;
+    }
 
-        //since from the last so nth node will be first
-        if(count==n){
-            ListNode *newHead = head->next;
-            delete head;
-            return newHead;
+    ListNode*getPrevVal(ListNode*head, int n){
+        for(int i=0; i<n-1; i++){
+            head = head->next;
         }
-
-        int res = count - n;
-        temp = head;
-        while(temp!=NULL){
-            res--;
-            if(res==0) break;
-            temp = temp->next;
-        }
-        
-        ListNode *delNode = temp->next;
-        temp->next = temp->next->next;
-        delete delNode;
         return head;
-        */
-
-        //better way --
-        /*
-        The dummy node is a "helper node" placed before the head:
-        Reason:
-        Edge cases: If we need to remove the first node (n = length of list),
-        there is no previous node to update next.
-        With dummy, the node before the one to delete is always accessible (slow will point to it).
-        It makes the code uniform — we don’t need separate logic for deleting the head.
-        */
-        ListNode *dummy = new ListNode(0,head);
-        ListNode *fast = dummy;
-        ListNode *slow = dummy;
-
-        //move n+1 
-        /*
-        Why move fast n+1 steps ahead?
-        fast starts at dummy.
-        slow starts at dummy.
-        Moving fast n+1 steps ensures that the distance between slow and fast is n+1 nodes.
-        Key idea:
-        When fast reaches the end (NULL), slow is exactly one node before the node to delete.
-        This works because the gap ensures slow->next is the nth node from the end.
-        */
-        for(int i=0; i<=n; i++){
-            fast = fast->next;
-        }
-        while(fast!=NULL){
-            fast = fast->next;
-            slow = slow->next;            
-        }
-        ListNode *delNode = slow->next;
-        slow->next = slow->next->next;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head==NULL) return head;
+        if(n==1 && head->next==NULL) return head->next;
+        int len = countLen(head);   
+        int k = len-n;        
+        if(k==0) return head->next;
+        ListNode*prevVal = getPrevVal(head, k);        
+        ListNode *delNode = prevVal->next;
+        prevVal->next = delNode->next;        
         delete delNode;
-        return dummy->next;
+        return head;        
     }
 };

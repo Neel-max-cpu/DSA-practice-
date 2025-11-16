e07f99fe01df79c2b1a6a9839a366ccf50100297
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-11-16 20:04:53 +0530

    Time: 4 ms (15.95%), Space: 8.7 MB (100%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    void find(vector<int>&arr, int i, set<vector<int>>&s, vector<int>&temp){
-        if(i==arr.size()){
-            if(s.find(temp)==s.end()){
-                //if not found
-                s.insert(temp);
-            }
+
+    void helper(vector<int>&nums, int n, vector<int>&ds, set<vector<int>>&ans, int idx){
+        // base case
+        if(idx == n){
+            ans.insert(ds);
             return;
         }
-        temp.push_back(arr[i]);
-        find(arr, i+1, s, temp);
-        temp.pop_back();
-        find(arr, i+1, s, temp);
+
+        // pick the ele
+        ds.push_back(nums[idx]);
+        helper(nums, n, ds, ans, idx+1);
+        ds.pop_back();
+
+        // don't pick the ele
+        helper(nums, n, ds, ans, idx+1);
     }
 
-    vector<vector<int>> subsetsWithDup(vector<int>& arr) {   
-        sort(arr.begin(), arr.end());     
-        set<vector<int>>s;
-        vector<int>temp;
-        find(arr, 0, s, temp);
-        vector<vector<int>>ans(s.begin(), s.end());
+    void helper2(vector<int>&nums,int n, vector<int>&ds, vector<vector<int>>&ans, int idx){
+        ans.push_back(ds);
+        for(int i=idx; i<n; i++){
+            if(i!=idx && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            helper2(nums, n, ds, ans, i+1);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
+        // taking extra space and time --  set and time to convert set to vector
+        /*
+        vector<int>ds;
+        set<vector<int>>ans;
+        sort(nums.begin(), nums.end());
+        helper(nums, n, ds, ans, 0);
+        vector<vector<int>> res;
+        res.insert(res.end(), ans.begin(), ans.end());
+        return res;
+        */
+
+        vector<int>ds;
+        vector<vector<int>>ans;
+        sort(nums.begin(), nums.end());
+        helper2(nums, n, ds, ans, 0);
         return ans;
     }
 };

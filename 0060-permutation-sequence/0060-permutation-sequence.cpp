cfa2a96ede7ad56a3f31db7a872905ca1d9b74ad
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-11-25 21:16:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (74.26%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,7 +1,38 @@
 class Solution {
-public:
+public:    
+    int totalPermutation(int n){
+        int ans = 1;
+        for(int i=1; i<=n; i++){
+            ans *= i;
+        }
+        return ans;
+    }
+    void findKth(string &temp, string &s, int n, int k){
+        if(temp.size()==1){
+            s+=temp[0];
+            return;
+        }
+
+        // n-1 since chunks if n=4 then 1+{} ; 2+{} ; 3+{} ; 4+{}
+        // here in {} we have 3 element option ans 3 element can have 3! = 6ways
+        // so each chunks have 6 ways
+        // so permutations of n-1
+        int totalChunks = totalPermutation(n-1);        
+        int index = k/totalChunks;
+        
+        //put the value in s
+        s+= temp[index];
+        // since temp is already sorted and for the next we don't need the temp[index]         
+        // Deletes 1 char starting from index = index
+        temp.erase(index, 1);
+        // or - deletes a char at the pos index   
+        // temp.erase(temp.begin() + index); 
+                
+        findKth(temp, s, n-1, k%totalChunks);
+    }
+
     string getPermutation(int n, int k) {
-        // /*
+        /*
         string s="";
         for(int i=1; i<=n; i++){
             s+=to_string(i);
@@ -11,6 +42,14 @@ public:
             next_permutation(s.begin(), s.end());
         }
         return s;
-        // */
+        */
+        // 0th based so looking for k-1th based permutation ---
+        string temp = "";
+        for(int i=1; i<=n; i++){
+            temp+=to_string(i);
+        }  
+        string s="";
+        findKth(temp,s, n, k-1);
+        return s;
     }
 };

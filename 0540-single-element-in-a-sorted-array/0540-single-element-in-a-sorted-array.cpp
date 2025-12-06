1a6b21ae23603b20cf2f31a9d3c17de301d735ac
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-12-06 11:06:53 +0530

    Time: 12 ms (4.13%), Space: 25 MB (99.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,30 +1,94 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        // pair starts with even/odd index 
-        // after single element it become odd/even index
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
 
-        // if mid is even then check for mid+1
-        // if mid is odd then check for mid-1
-        // if condition breaks check in the left half else go right
+        // optimised binary search --
+        /*
+        1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6 -- eg
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        see -- generally its even odd same number, 0=1(1); 2=3(2); 4=5(3);
+        but when a single element is occured then it become reversed 7=8(5); 9=10(6);
+        so even=odd if left half and odd=even if right half in binary search
+        that's why mid%2==0 if mid index is even check for i+1(odd) and if mid+2==1 if mid index is odd check for i-1(even) to check for 
+        the left half, cause left half genearlly stays the same and we know the single element is in the righ half so low = mid+1;
+        */
 
-        int n = arr.size();
-        if(n==1) return arr[0];
-        
-        int low = 0;
-        int high = n-1;
+        // edge cases, if size=1, first ele and last ele
+        if(n==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        int low = 1;
+        int high = n-2;
         while(low<=high){
-            int mid = low + (high-low)/2;
-            if(mid==0 && arr[mid]!=arr[mid+1]) return arr[mid];
-            else if(mid==n-1 && arr[mid]!=arr[mid-1]) return arr[mid];
-            else if(mid>0 && mid<n-1 && arr[mid]!=arr[mid+1] && arr[mid]!=arr[mid-1]) return arr[mid];
-            else if(arr[mid]==arr[mid^1]) {         //correct pair
-                // if mid is even then arr[mid]==arr[mid+1]
-                // if mid is odd then arr[mid]==arr[mid-1]
-                low = mid+1;
+            int mid = (low+high)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+            else{
+                if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1])) low = mid+1;
+                else high = mid-1;
             }
-            else if(arr[mid]!=arr[mid^1]) high = mid-1;
         }
         return -1;
+
+
+        // using xor  a^a = 0 and a^0 = a;
+        // so what will happen ---
+        /*
+        1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6 -- eg
+        0 ^1 = 1; 1^1 = 0; then again 0^2 = 2; 2^2 = 0; then again 0^3 = 3; 3^3 = 0;
+        then again 0^4 = 4; 4^5(lets keep it without finding the value); then 4^5^5 = 4(since 5^5 cancel);
+        then 4^6 = 4^6; then 4^6^6 = 4 again so return 4 done;
+        */
+        /*
+        int ans = 0;
+        for(int i = 0;i <n; i++){
+            ans = ans^nums[i];
+        }
+        return ans;
+        */
+        // brute
+        /*
+        if(n==1) return nums[0];
+        for(int i=0; i<n; i++){
+            // first index
+            if(i==0) {
+                if(nums[i]!=nums[i+1]) return nums[i];
+            }
+            // last index
+            else if(i==n-1){
+                if(nums[i]!=nums[i-1]) return nums[i];
+            }
+
+            else{
+                if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) return nums[i];
+            }
+        }
+        return -1;
+        */
+
+        // bruh solution --
+        /*
+        if(n==1) return nums[0];
+        int count = 1;
+        int ans = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i]!=nums[i-1]){
+                // cout<<i<<" "<<count<<endl;
+                if(count==2){
+                    count = 1;
+                    // continue;
+                }
+                else if(count==1){
+                    count = 0;
+                    ans = nums[i-1];
+                    break;
+                }
+            }
+            else count++;
+        }
+        if(count==1) ans = nums[n-1];
+        return ans;
+        */
     }
 };

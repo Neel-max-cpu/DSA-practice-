c511f4a1e4c1aa15e2a6dfa828b869a9c8f33320
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-12-17 22:27:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& arr, int target) {
-        int m = arr.size();
-        int n = arr[0].size();
-        int low = 0, high = m*n-1;
 
-        // we could do mid/m and mid%m if it were column wise sorted(but here its row wise)
+    bool findTarget(vector<int>&arr, int k){
+        int low = 0, high = arr.size()-1;
         while(low<=high){
-            // int mid = (low+high)/2;
             int mid = low + (high-low)/2;
-            int midRow = (mid/n);
-            int midCol = (mid%n);
-            if(target == arr[midRow][midCol]){
-                return true;
-            }
-            else if(target>arr[midRow][midCol]){
+            if(arr[mid]==k) return true;
+            else if(arr[mid]<k){
                 low = mid+1;
             }
             else{
                 high = mid-1;
             }
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int n = arr.size();
+        int m = arr[0].size();
+        /*
+        for(int i=0; i<n; i++){
+            int first = arr[i][0];
+            int last = arr[i][m-1];
+            if(first<=target && target<=last){
+                return findTarget(arr[i], target);
+            }
+        }
+        return false;
+        */
+
+        //optimal --
+        int low = 0, high = (n*m)-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int i = mid/n;
+            int j = mid%n;
+            int x = arr[i][j];
+            if(x==target) return true;
+            else if(x<target){
+                low = mid+1;
+            }
+            else high = mid-1;
         }
         return false;
     }

919f9bd291d969310d88a7e0678e92dbb61bfc24
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-12-19 08:09:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    double calculatePow(double x, long long n){
-        if(n==1) return x;
-
-        if(n%2==0){
-            //even
-            return calculatePow(x*x, n/2);
-        }
-        else {
-            //odd
-            double val = calculatePow(x, n-1);
-            return x*val;
-        }
-    }
-
     double myPow(double x, int n) {
-        long long N = n;
-        if(n<0) {
-            N = (long long)-1 * (long long)n;
-            x = 1/x;
+        // return pow(x, n);
+        long long nn = n;
+        double ans = 1.0;
+        if(n<0){
+            nn *= -1;
         }
 
-        if(n==0) return 1.0;
-        if(n==1) return x;
-
-        return calculatePow(x, N);
-
-
+        while(nn>0){
+            if(nn%2==0){
+                // if even square
+                x = x*x;
+                nn/=2;
+            }
+            else{
+                //if odd
+                ans *=x;
+                nn--;
+            }
+        }
+        if(n<0) return double(1.0)/ans;
+        else return ans;
 
     }
 };

2170c6d531ed447e37e03519ec8897bed55a8e1e
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-01-07 22:34:27 +0530

    Time: 0 ms (100%), Space: 14.7 MB (61.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
-        //brute - o(n^2) - two loops
-        // optimize o(nlogn) - sort then 2 pointers
-        // best hashmap?
         int n = arr.size();
         unordered_map<int,int>m;
-        for(int i = 0; i<n; i++){
+        for(int i=0; i<n; i++){
             int x = arr[i];
-            int left = target-x;
+            int left = target - x;
             if(m.find(left)!=m.end()){
-                //if found
-                return {i, m[left]};
+                //found
+                return {i,m[left]};
             }
             else{
-                //put the element in the map
                 m[x] = i;
             }
         }
-        return{-1,-1};
+        return {-1,-1};
     }
 };

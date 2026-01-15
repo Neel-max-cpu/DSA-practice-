87cca5350f3561909f2735372e688c6c8b651e80
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-01-15 22:21:13 +0530

    Time: 23 ms (51.18%), Space: 17.3 MB (98.14%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,32 +4,24 @@ public:
         int n = arr.size();
         sort(arr.begin(), arr.end());
         vector<vector<int>>ans;
-        for(int i=0; i<n; i++){
-            if(i>0 && arr[i]==arr[i-1]){
-                //skip duplicates
-                continue;
-            }
-            for(int j=i+1; j<=n-3;j++){
-                if(j>i+1 && arr[j]==arr[j-1]){
-                    //skip duplicates
-                    continue;
-                }
-                int k = j+1, l = n-1;
+        for(int i=0; i<n-3; i++){
+            if(i>0 && arr[i]==arr[i-1]) continue;            
+            for(int j=i+1; j<n-2;j++){
+                if(j>i+1 && arr[j]==arr[j-1]) continue;                
+                int k = j+1,l=n-1;
                 while(k<l){
                     long long sum = arr[i];
-                    sum += arr[j];
-                    sum += arr[k];
-                    sum += arr[l];
-                    if(sum==target){
+                    sum+=arr[j];
+                    sum+=arr[k];
+                    sum+=arr[l];
+                    if(sum == target){
                         ans.push_back({arr[i], arr[j], arr[k], arr[l]});
-                        k++; l--;
-                        //to avoid duplicates
-                        while(k<l && arr[k]==arr[k-1]) k++;
-                        while(l>k && arr[l]==arr[l+1]) l--;
-                    }
-                    else if(sum>target){
+                        k++;
                         l--;
+                        while(k<l && arr[k]==arr[k-1]) k++;
+                        while(l>k && arr[l]==arr[l+1]) l--;                    
                     }
+                    else if(sum>target) l--;
                     else k++;
                 }
             }

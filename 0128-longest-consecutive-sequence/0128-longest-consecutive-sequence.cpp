ba8d6bc084f00ce107ff60f9d6018040edd899c2
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-01-21 23:17:09 +0530

    Time: 62 ms (78.01%), Space: 49.2 MB (100%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,24 +1,86 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        // better ig - sorting(nlogn+n)
-        //optimized
-        unordered_set<int>st(nums.begin(), nums.end());
-        int count = 0, longest = 0;
-        for(auto it:st){
-            int x = it;
-            if(st.find(it-1)==st.end()){
-                //could be starting                
-                count = 1;
-                int curr = it;
-                while(st.find(curr+1)!=st.end()){
-                    //while there are next element
-                    count++;
-                    curr++;
-                }
-                longest = max(count, longest);
-            }            
+    int longestConsecutive(vector<int>& arr) {
+        int n = arr.size();
+        
+        if(n==0) return 0;
+
+
+        // kinda brute force --- time - o(n) + o(n*n = n2) and space = o(n)
+        // or can go in a loop, and save x and do a linear search each time to find the next
+        /*
+        unordered_map<int,int>m;
+        for(int i=0;i<n; i++) {
+            m[arr[i]]++;
         }
-        return longest;
+
+        int ans = 0, count = 1;
+        for(int i=0; i<n; i++){
+            int x = arr[i];
+            x++;
+            while(m.find(x) != m.end()){
+                count++;
+                x++;
+            }
+            ans = max(ans, count);
+            count = 1;
+        }
+        return ans;
+        */
+
+        // better sol
+        // /*
+        sort(arr.begin(), arr.end());
+        int current = 0, last_smaller = INT_MIN;
+        int count = 1;
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            current = arr[i];
+            if(current==last_smaller+1){
+                count++;
+                last_smaller = current;
+            }
+            else if(current==last_smaller){
+                continue;
+            }
+            else{
+                ans = max(count, ans);
+                last_smaller = current;
+                count = 1;
+            }
+        }
+        ans = max(count, ans);
+        return ans;
+        // */
+
+        // best sol ------ similar to 1st one but we don't check for most of them
+        /*
+        unordered_set<int>s;
+        for(int i=0; i<n; i++){
+            s.insert(arr[i]);
+        }
+
+        int ans = 1;
+        for(auto it:s){
+            int x = it -1;
+            if(s.find(x) != s.end()){
+                // if we find the previous ele we continue, since we want to start from the 
+                // first ele
+                continue;
+            }
+            else{
+                // may be the first ele so we start checking from here
+                int count = 1;
+                x = it+1;
+                while(s.find(x)!=s.end()){
+                    x++;
+                    count++;
+                }
+                ans = max(ans, count);
+                count = 1;
+            }
+        }
+        return ans;
+        */
     }
 };

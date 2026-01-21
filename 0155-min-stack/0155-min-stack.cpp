1f9e63a96498621b054725261a7b94de9d9b020f
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-01-21 22:16:55 +0530

    Time: 4 ms (43.61%), Space: 23.4 MB (62.64%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,32 +1,61 @@
 class MinStack {
 public:
     stack<int>st;
+    
+    // brute ---
+    /*
     priority_queue<int,vector<int>,greater<int>> pq;
+    */
+
+    // better ---
+    long minE;    
+    stack<int>mini;
     MinStack() {
-        
+        // very large value
+        minE = LONG_MAX;
     }
     
     void push(int val) {
-        st.push(val);
+        // brute ---
+        /*        
         pq.push(val);
+        */
+        minE = min(minE, (long)val);            
+        st.push(val);
+        mini.push(minE);
     }
     
     void pop() {
+        // brute ---
+        /*
         int x = st.top();
-        popQ(x);
+        popQ(x);        
+        */
         st.pop();
+        mini.pop();
+        if(mini.empty()) minE = LONG_MAX;        
+        else{
+            minE = mini.top();
+        }
     }
     
-    int top() {
+    int top() {                
         if(st.empty()) return -1;
-        else return st.top();
+        else return st.top();           
     }
     
     int getMin() {
+        // brute ---
+        /*
         if(pq.empty()) return -1;
         else return pq.top();
+        */
+        if(mini.empty()) return -1;
+        else return mini.top();
     }
 
+    // brute ---
+    /*
     private:
     void popQ(int x){
         vector<int>temp;
@@ -40,6 +69,7 @@ public:
 
         for(auto it:temp) pq.push(it);
     }
+    */
 };
 
 /**

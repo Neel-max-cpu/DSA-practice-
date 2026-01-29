28c9b813d63c14607180134a5bc1f5a32e45e535
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-01-29 23:06:27 +0530

    Time: 35 ms (43.99%), Space: 92.1 MB (58.65%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,13 +1,40 @@
 class StockSpanner {
 public:
-    vector<int>v;    
+    // brute ---
+    // vector<int>v;    
+
+    //optimal ---
+    stack<pair<int,int>>st;
+    int idx;
     StockSpanner() {        
+        idx = 0;
     }
     
     int next(int price) {
+        // brute
+        /*
         v.push_back(price);
         int span = findSpan(v);
         return span;
+        */
+      
+        while(!st.empty() && st.top().first<=price){
+            // while the top ele is <= price pop -- since we need to keep the stack in decreasing order
+            st.pop();
+        }
+        int span = 0;
+        if(st.empty()){
+            // basically idx - (-1) considering empty = -1 (eg at 0 st is empty idx 0 - (-1) = 1ele)
+            span = idx+1;            
+        }
+        else{
+            // not right - left+1 since we don't include the top ele(since we need) <=
+            span = idx-st.top().second;             
+        }    
+        st.push({price, idx});
+        idx++;
+        return span;
+
     }
 
     private:

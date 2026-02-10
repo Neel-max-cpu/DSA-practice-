efa389e83cf873812732fcbca7063242d55655ea
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-02-10 22:39:56 +0530

    Time: 0 ms (100%), Space: 11.9 MB (44.45%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
-        for(int i=0; i<strs.size(); i++){
-            if(i==0) {
-                ans = strs[i];
-                // cout<<ans<<" "<<i<<endl;
+    string compare(string &one, string &two){
+        int n = one.size();
+        int m = two.size();
+        int len = min(n, m);
+        
+        int i = 0;
+        for(i=0; i<len; i++){            
+            if(one[i]!=two[i]) break;
+        }
+        
+        // similar from 0 to i-1
+        if(i==len){
+            // just for better tc since substring creation is o(k) where k = len
+            if(n>m) return two;
+            else return one;
+        }     
+        else{
+            return one.substr(0, i);
+        }   
+    }
+
+    string longestCommonPrefix(vector<string>& arr) {
+        int n = arr.size();
+
+        // vertical scaning ---
+        /*
+        int minLen = INT_MAX;
+        for(int i=0; i<n; i++){
+            int l = arr[i].size();
+            minLen = min(l, minLen);
+        }
+
+        int j=0;
+        string s ="";
+        char c = arr[0][j];
+        while(j<minLen){
+            for(int i=1; i<n; i++){
+                if(arr[i][j]!=c) return s;
             }
-            else{
-                string temp = "";
-                int j = 0;
-                int k = 0;
-                while(k<ans.size() && j<strs[i].size()){
-                    if(strs[i][j]==ans[k]){
-                        k++;
-                        j++;
-                    }
-                    else break;
-                }
-                ans.erase(k, ans.size()-k);
-                if(ans.size()==0) return ans;
-                // cout<<ans<<" "<<i<<endl;                
-            }
+            s+=c;
+            j++;
+            if(j<minLen)
+                c = arr[0][j];
+        }
+        return s;
+        */
+
+        // horizontal scaning --- assume first string is the prefix
+        string ans = arr[0];
+        for(int i=1; i<n; i++){            
+            string val = compare(ans, arr[i]);
+            ans = val;
         }
         return ans;
+
     }
 };

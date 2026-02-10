0d1d6c6f0f2301bec20249701ff3e34e96883277
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-02-10 22:08:03 +0530

    Time: 0 ms (100%), Space: 9.3 MB (11.44%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        int n = s.size();
         int i = 0;
+        while(s[i]==' ') i++;
+        
         int sign = 1;
-        long ans = 0;
-        
-        // remove starting spaces
-        while(i<s.size() && s[i]==' ')
-            i++;    
-        
-        if(s[i]=='-'){
+        if(s[i]=='-') {
             sign = -1;
             i++;
         }
-        else if(s[i]=='+'){
+        else if(s[i]=='+') i++;
+
+        long val = 0;
+        bool first = true;
+        while(i<n){
+            if(s[i]>='0' && s[i]<='9') {
+                if(!first) val*=10;
+                val+=s[i]-'0';   
+                if(val>=INT_MAX) break;
+                first = false;           
+            }
+            else break;
             i++;
         }
-        while(i<s.size()){
-            if(s[i]>='0' && s[i]<='9'){
-                ans = ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign == -1) return INT_MIN;
-                else if(ans>INT_MAX && sign == 1) return INT_MAX;
-                i++;
-            }
-            // if found another char
-            else return ans*sign;
+        if(val>=INT_MAX){            
+            if(sign==1) return INT_MAX;
+            else{
+                if(val*sign > INT_MIN) return val*sign;
+                else return INT_MIN;
+            }            
         }
-        return ans*sign;
+        else{
+            return sign==1? val : val*sign;
+        }
     }
 };

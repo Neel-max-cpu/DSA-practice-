c555acb724fc56ed5f9b3857b93766c96f7015f1
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-02-18 22:16:24 +0530

    Time: 0 ms (100%), Space: 9.4 MB (16.84%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -60,6 +60,63 @@ public:
         return ans;
     }
 
+    vector<int>kmpAlgo(string &a, string &b){
+        int n = a.size();
+        int m = b.size();
+
+        // build lps for pattern --
+        /*
+        length of the longest proper prefix of pattern[0…i]
+        that is also a suffix of pattern[0…i]
+        *Proper prefix = cannot be the whole string.
+        */
+        vector<int>lps(m,0);
+        int i = 1;
+        int len = 0;
+        while(i<m){
+            if(b[i]==b[len]){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if(len!=0){
+                    len = lps[len-1];
+                }
+                else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+
+        // pattern matching
+        i=0;
+        int j=0;
+        vector<int>ans;
+        while(i<n){
+            if(a[i]==b[j]){
+                i++;
+                j++;
+            }
+            if(j==m){
+                // starting index will be i-m -- i++ and j++ already happned
+                ans.push_back(i-m);
+                j=lps[j-1];
+            }
+
+            else if(i<n && a[i]!=b[j]){
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+
     int findIndex(string &a, string &b){
         int n = a.size();
         int m = b.size();
@@ -69,7 +126,7 @@ public:
         vector<int>ans;
         // method 1 -- worst case o(n*m)
         /*
-        for(int i=0; i<n-m; i++){
+        for(int i=0; i<=n-m; i++){
             int j = 0;
             while(j<m && a[i+j]==b[j]) j++;
             if(j==m){
@@ -99,9 +156,10 @@ public:
 
 
         // better method -- o(n+m) worst case o(n*m) --rabin karp algorithm
-        ans = rabinKarpAlgo(a,b);
+        // ans = rabinKarpAlgo(a,b);
 
         // best -- kmp and z function(dont know z function todo kmp)
+        ans = kmpAlgo(a,b);
 
         return ans.empty()? -1 : ans[0];
     }

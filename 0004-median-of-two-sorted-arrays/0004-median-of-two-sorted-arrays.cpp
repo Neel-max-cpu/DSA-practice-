e60582d32387b5c44edc1a3b17f015c4e0b7158d
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-02-18 19:23:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,136 +1,192 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+    double betterSol(int n, int m, int len, vector<int>&arr1, vector<int>&arr2){
+        if(len%2==1){
+            // odd mid ele
+            int mid = len/2;            
+            int i = 0, j=0;   
+            int count=0;         
+            while(i<n && j<m){                
+                if(count==mid){
+                    double ans = min(arr1[i], arr2[j]);
+                    return ans;                    
+                }
+                if(arr1[i]<=arr2[j]) i++;
+                else j++;
+                count++;
+            }
 
-        // do binary search on smaller array
-        if(n>m) return findMedianSortedArrays(nums2, nums1);
+            while(i<n){
+                if(count==mid){
+                    return double(arr1[i]);
+                }
+                i++;
+                count++;
+            }
 
-        int len = n+m;
-        // length of the left half
-        int left = (n+m+1)/2;
+            while(j<m){
+                if(count==mid){
+                    return double(arr2[j]);
+                }
+                j++;
+                count++;
+            }
+        }
+        else{
+            // even
+            int mid = len/2;            
+            int i = 0, j=0;   
+            int count=0;         
+            double first = 0.0;
+            double second = 0.0;
+            while(i<n && j<m){                
+                if(count==mid-1){
+                    vector<int>v(4,INT_MAX);
+                    v[0] = arr1[i];
+                    v[1] = arr2[j];
+                    if(i+1<n){
+                        v[2] = arr1[i+1];
+                    }
+                    if(j+1<m){
+                        v[3] = arr2[j+1];
+                    }
+                    int one = INT_MAX, two = INT_MAX;
+                    for(int i=0; i<4; i++){
+                        if(v[i]<one){
+                            two = one;
+                            one = v[i];
+                        }
+                        else if(v[i]<two){
+                            two = v[i];
+                        }
+                    }
 
-        int low = 0, high = n;
-        while(low<=high){
-            int mid1 = (low+high)/2;
-            int mid2 = left-mid1;
+                    first = one;
+                    second = two;
 
-            // calculate l1, l2, r1, and r2 initiallize with INT_MAX,MIN if they are not there
-            int l1 = INT_MIN, l2 = INT_MIN;
-            int r1 = INT_MAX, r2 = INT_MAX;
-            
-            // if the index doens't exist so check boundary and put the elements in l/r
-            if(mid1 < n) r1 = nums1[mid1];
-            if(mid2 < m) r2 = nums2[mid2];
-            if(mid1-1>=0) l1 = nums1[mid1-1];
-            if(mid2-1>=0) l2 = nums2[mid2-1];
+                    double ans = (first+second)/(double)2;
+                    return ans;
+                }
+                if(arr1[i]<=arr2[j]) i++;
+                else j++;
+                count++;
+            }
 
-            if(l1<=r2 && l2<=r1){
-                if(len%2==1) return max(l1, l2);
+            while(i<n){
+                if(count==mid-1){
+                    first = arr1[i];
+                    second = arr1[i+1];
+                    double ans = (first+second)/(double)2;
+                    return ans;
+                }
+                i++;
+                count++;
+            }
+
+            while(j<m){
+                if(count==mid-1){
+                    first = arr2[j];
+                    second = arr2[j+1];
+                    double ans = (first+second)/(double)2;
+                    return ans;
+                }
+                j++;
+                count++;
+            }
+        }
+        return 0.0;
+    }
+
+    double betterSol2(int n, int m, int len, vector<int>&arr1, vector<int>&arr2){
+        int median1 = 0, median2 = 0;
+        int mid = len/2;
+        int i = 0, j=0;
+        for(int count = 0; count<=mid; count++){
+            // each time when loop works update median2 since 
+            // it would contain 2nd smallest ele; and always update median1
+            median2 = median1;
+
+            if(i<n && j<m){
+                if(arr1[i] > arr2[j]){
+                    median1 = arr2[j];
+                    j++;
+                }
                 else {
-                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;                   
+                    median1 = arr1[i];
+                    i++;
                 }
             }
-
-            else if(l1>r2) high = mid1-1;
-            else low = mid1+1;
+            else if(i<n){
+                median1 = arr1[i];
+                i++;
+            }
+            else if(j<m){
+                median1 = arr2[j];
+                j++;
+            }
         }
-        return 0;
-        
-        
 
-        // better ---
-        /*
+        if(len%2==1){
+            return double(median1);
+        }
+        else{
+            double ans = (median1+median2)/(double)2;
+            return ans;
+        }
+    }
+
+    double optimal(vector<int>&arr1, vector<int>&arr2, int len){
+        int n = arr1.size();
+        int m = arr2.size();
+        // take 0 elements; take all(n) elements
+        int left = 0, right = n;
+        while(left<=right){
+            // mid represents how many eles we can take from arr1 into left partition
+            int mid = left + (right-left)/2;
+            /* 
+            so left eles + right eles = n+m
+            total elements on the leftSide = (n+m+1)/2; so if we take mid number of eles in 
+            the left part from arr1 then we need to take leftSide-mid elems from arr2
+            */
+            int leftArr1 = INT_MIN, leftArr2 = INT_MIN;
+            int rightArr1 = INT_MAX, rightArr2 = INT_MAX;
+            // total elements in left 
+            int totalInLeft = (len+1)/2;    // handle both odd/even 
+
+            // partition index in arr2            
+            int eleInArr2  = totalInLeft - mid;
+
+            int indexArr1 = mid-1;
+            int indexArr2 = eleInArr2-1;
+
+            if(indexArr1>=0) leftArr1 = arr1[indexArr1];
+            if(indexArr2>=0) leftArr2 = arr2[indexArr2];
+            if(indexArr1<n) rightArr1 = arr1[indexArr1+1];
+            if(indexArr2<m) rightArr2 = arr2[indexArr2+1];
+
+            // boundary check ---
+            if(leftArr1<=rightArr2 && leftArr2<=rightArr1){
+                // true partition found
+                
+            }
+            else if(leftArr1>rightArr2) left = mid+1;
+            else if(leftArr2>rightArr1) right = mid-1;
+        }
+    }
+
+    double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
+        int n = arr1.size();
+        int m = arr2.size();
         int len = n+m;
-        int ind2 = len/2;
-        int ind1 = ind2-1;
 
-        int i = 0, j =0;
-        int count = 0;
-        int mid1 = 0, mid2 = 0;
-        while(i<n && j<m){                
-            if(nums1[i]<nums2[j]){
-                if(count==ind1) mid1 = nums1[i];
-                if(count == ind2) mid2 = nums1[i];
-                i++;
-                count++;
-            }
-            else{
-                if(count==ind1) mid1 = nums2[j];
-                if(count==ind2) mid2 = nums2[j];
-                j++;
-                count++;
-            }
-        }
+        // o(m+n) -- like merging but doing directly
+        //return betterSol(n,m,len, arr1, arr2);
+        // return betterSol2(n,m,len, arr1, arr2);   
 
-        while(i<n){
-            if(count==ind1) mid1 = nums1[i];
-            if(count==ind2) mid2 = nums1[i];
-            i++;
-            count++;
-        }
-        while(j<m){
-            if(count==ind1) mid1 = nums2[j];
-            if(count==ind2) mid2 = nums2[j];
-            j++;
-            count++;
-        }
+        // optimal -- o(log(m+n))
+        // for tc take the smaller array ---
+        if(n>m) return findMedianSortedArrays(arr2, arr1);
+        return optimal(arr2, arr1, len);
 
-
-        if(len%2 == 1){
-            return mid2;
-        }
-        else{
-            return (double)((double)(mid1+mid2)/2.0);
-        }
-        */
-     
-
-        /*
-        vector<int>v;
-        int i=0, j=0;
-        while(i<n && j<m){
-            if(nums1[i]<nums2[j]){
-                v.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                v.push_back(nums2[j]);
-                j++;
-            }
-        }
-        while(i<n){
-            v.push_back(nums1[i++]);
-        }
-        while(j<m){
-            v.push_back(nums2[j++]);
-        }
-        double median = 0;
-        int len = v.size();
-        if(len%2==1) return (double)(1.00000 * v[len/2]);
-        else{
-            median = v[len/2] + v[len/2 -1];
-            median = (1.00000)*median/2;
-            return median;
-        }
-        */
-
-        // brute force ---
-        /*
-        vector<int>v(n+m,0);
-        int k = 0;
-        for(int i=0; i<n; i++) v[k++] = nums1[i];
-        for(int i=0; i<m; i++) v[k++] = nums2[i];
-        double median = 0;
-        int len = v.size();
-        sort(v.begin(), v.end());
-        if(len%2==1) return (double)(1.00000 * v[len/2]);
-        else{
-            median = v[len/2] + v[len/2 -1];
-            median = (1.00000)*median/2;
-            return median;
-        }
-        */
     }
 };

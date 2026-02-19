5937da8b3fa4e35839d28c8c4b52947995d06e62
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-02-19 21:04:38 +0530

    Time: 0 ms (100%), Space: 10.8 MB (65.95%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,80 +11,45 @@
  */
 class Solution {
 public:
-    void helper(TreeNode *root, vector<int>&ans){
-        if(root==NULL){
+    void inorder (TreeNode*root, vector<int>&ans){
+        if(root==NULL){           
             return;
         }
-        helper(root->left, ans);        
+        inorder(root->left, ans);
         ans.push_back(root->val);
-        helper(root->right,ans);
+        inorder(root->right, ans);
+
+    }
+    vector<int>iterative(TreeNode*root){
+        vector<int>ans;
+        stack<TreeNode*>st; 
+        TreeNode*current = root;       
+        while(current || !st.empty()){  
+            while(current){
+                st.push(current); 
+                current = current->left;           
+            }
+            current = st.top();
+            st.pop();
+            ans.push_back(current->val);
+            current = current->right;
+        }
+        return ans;
     }
 
-    vector<int> inorderTraversal(TreeNode* root) {    
-        // inorder - left root right
+    vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
+        // inorder -- left, root, right
         
-        // recursively ----
-        // helper(root, ans);
+        // recursive ---
+        // inorder(root, ans);
 
-        // iteratively --
-        /*
-        stack<TreeNode*>s;
-        TreeNode *current = root;
-        while(current != NULL || !s.empty()){
-            while(current!=NULL){
-                s.push(current);
-                current=current->left;
-            }
-            current = s.top();
-            s.pop();
-            ans.push_back(current->val);
-            current=current->right;
-        }                        
-        return ans;
-        */
-
-        // using morris traversal ---
-        TreeNode *current = root;
-        while(current !=NULL){
-            if(current->left==NULL){                
-                ans.push_back(current->val);
-                current=current->right;
-            }
-            else{
-                /*
-                          1
-                         / \
-                        2   3
-                       / \
-                      4   5
-                           \
-                            6
-                */
-                // left is not null -- take the left subtree's(2s) right most guy(6) and 
-                TreeNode *previous = current->left;
-                // 2 case if next is null or else current 
-                while(previous->right!=NULL && previous->right!=current){
-                    previous=previous->right;
-                }
+        // iterative ---
+        ans = iterative(root);
 
 
-                // connect it to current's node(if next is null) after connection is done,                 
-                // move to the left permananently
-                if(previous->right==NULL){
-                    // create the thread
-                    previous->right = current; 
-                    current = current->left;
-                }
-                // else remove the thread
-                else{
-                    // cut the thread
-                    previous->right = NULL;
-                    ans.push_back(current->val);
-                    current = current->right;
-                }                
-            }
-        } 
+
+
         return ans;
     }
 };

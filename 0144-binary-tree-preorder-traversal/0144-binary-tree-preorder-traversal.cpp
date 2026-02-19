a4768abf9ade77bf94143fe88a9703c33d8981b5
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-02-19 21:04:55 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.21%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,84 +11,42 @@
  */
 class Solution {
 public:
-    void helper(TreeNode *root, vector<int>&ans){
+    void preorder(TreeNode*root, vector<int>&ans){
         if(root==NULL) return;
+
         ans.push_back(root->val);
-        helper(root->left, ans);
-        helper(root->right, ans);
+        preorder(root->left, ans);
+        preorder(root->right, ans);
     }
-    vector<int> preorderTraversal(TreeNode* root) {
-        // preorder -- root left right
-        vector<int>ans;
 
-        // recursive ---
-        // helper(root, ans);
-
-        // iterative ---
-        /*
-        stack<TreeNode*>s;
+    vector<int>iterative(TreeNode *root){
+        stack<TreeNode*>st;
         TreeNode*current = root;
-
-        if(root!=NULL) s.push(root);
-        while(!s.empty()){
-            TreeNode *node = s.top();
-            s.pop();
-            ans.push_back(node->val);
-
-            // since stack so push the right child first to process the left one first
-            if(node->right!=NULL){
-                s.push(node->right);
+        vector<int>ans;
+        while(current || !st.empty()){
+            while(current){
+                st.push(current);
+                ans.push_back(current->val);
+                current = current->left;
             }
-            if(node->left != NULL){
-                s.push(node->left);
-            }
+            current = st.top();
+            st.pop();
+            current = current->right;
         }
         return ans;
-        */
+    }
 
-        // using morris traversal --- similar to inorder just 1 line change
-        TreeNode *current = root;
-        while(current !=NULL){
-            if(current->left==NULL){                
-                ans.push_back(current->val);
-                current=current->right;
-            }
-            else{
-                /*
-                          1
-                         / \
-                        2   3
-                       / \
-                      4   5
-                           \
-                            6
-                */
-                // left is not null -- take the left subtree's(2s) right most guy(6) and 
-                TreeNode *previous = current->left;
-                // 2 case if next is null or else current 
-                while(previous->right!=NULL && previous->right!=current){
-                    previous=previous->right;
-                }
+    vector<int> preorderTraversal(TreeNode* root) {
+        // preorder -- root, left, right
 
+        vector<int>ans;
+        // recursion
+        // preorder(root, ans);
 
-                // connect it to current's node(if next is null) after connection is done,                 
-                // move to the left permananently
-                if(previous->right==NULL){
-                    // create the thread
-                    previous->right = current; 
-                    // push the value when making the thread -- 
-                    ans.push_back(current->val);
-                    
-                    current = current->left;
-                }
-                // else remove the thread
-                else{
-                    // cut the thread
-                    previous->right = NULL;
-                    current = current->right;
-                }                
-            }
-        } 
+        // iterative --
+        ans = iterative(root);
+
         return ans;
+                
     }
 };

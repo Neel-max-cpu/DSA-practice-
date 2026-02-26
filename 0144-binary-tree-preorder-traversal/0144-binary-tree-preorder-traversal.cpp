2fa0777cf435d05d4c8d85f8726c189edd0b1834
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-02-26 21:51:41 +0530

    Time: 0 ms (100%), Space: 10.7 MB (99.69%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -36,6 +36,42 @@ public:
         return ans;
     }
 
+    vector<int>morris(TreeNode*root){
+        TreeNode*curr = root;
+        vector<int>ans;
+        while(curr){
+            if(curr->left == NULL){
+                // if no left child
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{                
+                // if left child is present then go to the left then the right most 
+                // node and connect the root to it
+                TreeNode*prev = curr->left;
+                while(prev->right && prev->right != curr){
+                    prev=prev->right;
+                }
+
+                // case 1 right is null then connect a thread
+                if(prev->right==NULL){
+                    prev->right = curr;
+                    // since we mark the thread so its the root and in the end we are printing the root
+                    ans.push_back(curr->val);
+                    // now we can do the traversal ---
+                    curr = curr->left;
+                }
+                else{
+                    // points to curr so disconnect the thread
+                    prev->right = NULL;
+                    // left is visited so, take curr and then go right                    
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+
     vector<int> preorderTraversal(TreeNode* root) {
         // preorder -- root, left, right
 
@@ -44,7 +80,10 @@ public:
         // preorder(root, ans);
 
         // iterative --
-        ans = iterative(root);
+        // ans = iterative(root);
+
+        // morris --
+        ans = morris(root);
 
         return ans;
                 

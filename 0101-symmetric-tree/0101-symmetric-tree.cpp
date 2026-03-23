f7a726ad7a5141d7bf95639783e669137a6b8bb6
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-03-23 22:38:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -22,7 +22,27 @@ public:
         return true;
     }
 
-    bool isSymmetric(TreeNode* root) {        
+    bool helper(TreeNode *root1, TreeNode*root2){        
+        if(!root1 || !root2){
+            // if both null return true else false
+            return root1==root2;
+        }
+
+        if(root1->val != root2->val) return false;
+
+        // go left(root1) and go right(root2)
+        bool one = helper(root1->left, root2->right);
+        // after returning go right(root1) and go left(root2)
+        bool two = helper(root1->right, root2->left);
+
+        // inorder traversal ---
+        return one&&two;
+    }
+
+    bool isSymmetric(TreeNode* root) {     
+
+        // brute ---
+        /*   
         queue<TreeNode*>q;
         if(root)
             q.push(root);
@@ -53,5 +73,11 @@ public:
             }
         }
         return true;
+        */
+
+        // optimal ---
+        // inorder traversal -- left, root, right (on root's left) and right, root, left(on root's right) should
+        // be same
+        return root == NULL || helper(root->left, root->right);
     }
 };

76c1d8cb4e981456d955985acc2cfdeab3c9ccc8
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-03-26 21:21:23 +0530

    Time: 11 ms (83.59%), Space: 19.5 MB (9.84%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -17,31 +17,33 @@ public:
 */
 
 class Solution {
-public:
+public:    
     Node* connect(Node* root) {
-        if(root==NULL) return NULL;
+        if(!root) return root;
+        
         queue<Node*>q;
         q.push(root);
+
         while(!q.empty()){
-            int size = q.size();
-            Node *prev = q.front();
-            q.pop();
-            if(prev->left!=NULL){
-                q.push(prev->left);
-                q.push(prev->right);
-            }
-            for(int i=1; i<size; i++){
-                Node *current = q.front();
+            int len = q.size();
+            for(int i=0; i<len; i++){
+                Node*curr = q.front();
                 q.pop();
-                prev->next = current;
-                current->next = NULL;
-                prev = current;
-                if(current->left!=NULL){
-                    q.push(current->left);
-                    q.push(current->right);
+                if(i==len-1){
+                    // last in that level
+                    curr->next = NULL;
                 }
+                else{
+                    // not last the point to next front
+                    curr->next = q.front();
+                }
+
+                // put left and right
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
             }
         }
+
         return root;
     }
 };

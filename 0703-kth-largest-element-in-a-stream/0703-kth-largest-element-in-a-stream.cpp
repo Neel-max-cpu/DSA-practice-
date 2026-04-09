c7a4509af3f9678cc7672c9830cd2fc1a7a9a5f0
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-04-09 23:19:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,24 +1,41 @@
 class KthLargest {
 public:
-    priority_queue<int>maxHeap;
-    priority_queue<int, vector<int>, greater<int>>minHeap;
+    int k;
+    priority_queue<int,vector<int>, greater<int>>minHeap;
+
     KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        // put all in minHeap - all in ascending order
         for(auto it:nums){
             minHeap.push(it);
-        }
-        // minHeap(nums.begin(), nums.end());
-        int temp = k-1;
-        while(minHeap.size()!=temp){
-            maxHeap.push(minHeap.top());
-            minHeap.pop();
-        }
+            // keep only k eles
+            if(minHeap.size()>k) minHeap.pop();
+        }        
+
+        /*
+        push 4 → heap = [4]
+        push 5 → heap = [4,5]
+        push 8 → heap = [4,5,8]
+        push 2 → heap = [2,4,5,8] → size > 3 → pop → remove 2
+        so, final -- [4, 5, 8]
+        */
     }
     
     int add(int val) {
+        /*
+        -- add 3
+        Heap before: [4,5,8]
+        push 3 → [3,4,5,8]
+        size > 3 → pop → remove 3 now [4,5,8] return 4
+
+        -- add 5 then [4,5,5,8] then pop 4 then [5,5,8] return 5
+        -- add 10 then [5,5,8,10] then pop 5 then [5,8,10] return 5
+        -- add 9 then [5,8,9,10] then pop 5 then [8,9,10] return 8
+        -- add 4 then [4,8,9,10] then pop 4 then [8,9,10] return 8 
+        */
         minHeap.push(val);
-        maxHeap.push(minHeap.top());
-        minHeap.pop();
-        return maxHeap.top();
+        if(minHeap.size()>k) minHeap.pop();
+        return minHeap.top();
     }
 };
 

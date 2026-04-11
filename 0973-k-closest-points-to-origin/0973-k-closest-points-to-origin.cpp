0636ccdd7bb31e1f0396f693805154ed6f265831
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-04-11 19:08:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -12,6 +12,7 @@ struct comp{
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        /*
         priority_queue<Neel*, vector<Neel*>, comp>minHeap;
         for(int i=0; i<points.size(); i++){
             int first = points[i][0];
@@ -35,6 +36,41 @@ public:
             int first = points[x->row][0];
             int second = points[x->row][1];
             ans.push_back({first, second});
+        }
+        return ans;
+        */
+
+        // little better -- don't need sqroot
+        /*
+        auto comp = [](const vector<int>& a, const vector<int>& b) {
+            return a[0]*a[0] + a[1]*a[1] > b[0]*b[0] + b[1]*b[1];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)>minHeap(comp);
+
+        for(auto it:points){
+            minHeap.push({it[0], it[1]});
+        }
+        vector<vector<int>>ans;
+        for(int i=0; i<k; i++){
+            ans.push_back(minHeap.top());
+            minHeap.pop();
+        }
+        return ans;
+        */
+        
+        // optimal -- using maxheap (only keep k eles) remove the maxHeap.top() -- will give the furthest
+        priority_queue<pair<int, pair<int,int>>> maxHeap;
+
+        for(auto it:points){
+            int dist = it[0]*it[0] + it[1]*it[1];
+            maxHeap.push({dist, {it[0], it[1]}});
+            if(maxHeap.size()>k) maxHeap.pop();
+        }
+
+        vector<vector<int>>ans;
+        while(!maxHeap.empty()){
+            ans.push_back({maxHeap.top().second.first,maxHeap.top().second.second});
+            maxHeap.pop();
         }
         return ans;
     }

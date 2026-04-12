7c64bd453e20d7e91349edaee0baaa667135e3ca
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-04-12 11:20:30 +0530

    Time: 0 ms (100%), Space: 24.4 MB (66.26%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,60 +11,23 @@
  */
 class Solution {
 public:
-    void helper(TreeNode*root, int& counter, int k, int &ans){
-        if(root==NULL || counter>=k) return;
-        helper(root->left, counter, k, ans);
-        counter++;
-        if(counter==k) {
-            ans = root->val;
-            return;
-        }
-        helper(root->right, counter, k, ans);
-    }
+    void helper(TreeNode *root, int k, int &ans, int &count){
+        if(!root) return;
 
-    void helper2(TreeNode*root, int& counter, int k, int &ans){
-        if(root==NULL || counter>=k) return;
-        helper2(root->right, counter, k, ans);
-        counter++;
-        if(counter==k) {
+        helper(root->left, k, ans, count);
+        count++;
+        if(count==k){
             ans = root->val;
             return;
         }
-        helper(root->left, counter, k, ans);
+        helper(root->right, k, ans, count);
+        
     }
 
     int kthSmallest(TreeNode* root, int k) {
-        // brute -- o(n)
-        /*
-        vector<int>ans;
-        // inorder - left root right
-        stack<TreeNode*>s;
-        TreeNode*current = root;        
-        while(!s.empty() || current!=NULL){
-            while(current!=NULL){
-                s.push(current);
-                current=current->left;
-            }
-            current = s.top();
-            s.pop();
-            ans.push_back(current->val);
-            current=current->right;
-        }
-        return ans[k-1];
-        */
-
-        // optimal - using counter -- k smallest
-        int ans= 0;
-        int counter = 0;
-        helper(root,counter,k, ans);
-        return ans;
-
-        // k largest 
-        /*   
         int ans = 0;
-        int counter = 0;
-        helper2(root, counter, k, ans);
+        int count = 0;
+        helper(root, k, ans, count);
         return ans;
-        */
     }
 };

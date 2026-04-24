0f18d8563a0143b5f18787fd9587dc424a0fb167
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-04-24 14:28:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -10,90 +10,128 @@
  * };
  */
 
-class NodeValue{
-public:
-    int minNode, maxNode, sum;
 
-    NodeValue(int minNode, int maxNode, int sum){
-        this->minNode = minNode;
+class Neel {
+    public:
+    int maxNode, minNode, sum;
+    Neel (int minNode, int maxNode, int sum){
         this->maxNode = maxNode;
-        this->sum = sum;        
+        this->minNode = minNode;
+        this->sum = sum;
     }
 };
 
 class Solution {
 public:
     int ans = 0;
-    bool isValid(TreeNode*root, long a, long b){
-        if(root==NULL) return true;
-        bool f1 = isValid(root->left, a, root->val);
-        bool f2 = isValid(root->right, root->val, b);
 
-        if((root->val>a && root->val<b) && f1 && f2) return true;
-        else return false;
+    int findSum(TreeNode *root){
+        if(!root) return 0;
+        int val = root->val;
+        int left=findSum(root->left);
+        int right = findSum(root->right);
+        return val+left+right;
     }
 
-    int find(TreeNode*root){        
-        queue<TreeNode*>q;
-        q.push(root);
-        int ans = 0;
-        while(!q.empty()){
-            for(int i=0; i<q.size(); i++){
-                TreeNode*current = q.front();
-                q.pop();            
-                ans += current->val;
-                if(current->left!=NULL) q.push(current->left);
-                if(current->right!=NULL) q.push(current->right);
-            }
+    bool isValid(TreeNode*root, int mini, int maxi){
+        if(!root) return true;
+
+        return (
+            (root->val > mini) && 
+            (root->val < maxi) &&
+            isValid(root->left, mini, root->val) &&
+            isValid(root->right, root->val, maxi)
+        );
+    }
+
+    pair<int,bool> betterIsValid(TreeNode*root, int mini, int maxi){
+        if(!root) return {0,true};
+
+        pair<int,bool>one, two, three, four;
+        if(root->val < mini) one = {root->val, false};
+        else one = {root->val, true};
+
+        if(root->val > maxi) two = {root->val, false};
+        else two = {root->val, true};
+
+        three = betterIsValid(root->left, mini, root->val);
+        four = betterIsValid(root->right, root->val, maxi);
+
+        if(one.second==true && two.second==true && three.second==true && four.second==true){
+            int val = root->val + three.first + four.first;
+            return {val, true};
         }
-        return ans;
+        else{
+            return {-1, false};
+        }        
     }
 
-    NodeValue helper(TreeNode*root){
-        if(root==NULL) return NodeValue(INT_MAX, INT_MIN, 0);
+    void check(TreeNode*root){
+        if(!root){
+            return;
+        }
+
+        // brute ---
+        /*
+        if(isValid(root, INT_MIN, INT_MAX)){
+            int val = findSum(root);
+            ans = max(val, ans);            
+        } 
+        */
+
+        // better ---
+        auto a = betterIsValid(root, INT_MIN, INT_MAX);
+        if(a.second){    
+            ans = max(a.first, ans);            
+        } 
+
+        check(root->left);
+        check(root->right);        
+    }   
+
+
+    Neel helper(TreeNode*root){
+        if(!root){
+            // valid so minNode would be max and maxNode would be min
+            return Neel(INT_MAX, INT_MIN, 0);
+        }
 
         auto left = helper(root->left);
         auto right = helper(root->right);
 
-        // if left's maximum < root's val < right's min then a valid bst
-        if(left.maxNode<root->val &&  root->val<right.minNode){
-            // min would be min of root, left's minimum
-            int minNode = min(root->val, left.minNode);
-            // max would be max of root, right maximum
-            int maxNode = max(root->val, right.maxNode);
-            int sum = left.sum+right.sum+root->val;
-            ans = max(sum, ans);            
-            return NodeValue(minNode, maxNode, sum);
+        if(left.maxNode < root->val && root->val < right.minNode){
+            int sum = left.sum + right.sum + root->val;
+            ans = max(ans, sum);
+            return Neel(
+                min(root->val, left.minNode),
+                max(root->val, right.maxNode),
+                sum
+            );
         }
-        // other wise return a very big number -- struct is (min, max, sum) 
-        // root's value can't be greater than left's max value(INT_MAX) and root's value can't be samller then rights' min value(INT_MIN)
-        return NodeValue(INT_MIN, INT_MAX, max(left.sum, right.sum));
-    }
+        // not valid -- then put minNode as min and maxNode as max 
+        return Neel(INT_MIN, INT_MAX, max({left.sum, right.sum}));
+    }      
+
+
 
     int maxSumBST(TreeNode* root) {
-        if(root==NULL) return 0;
-        // brute -- o(n^2)
+        // brute --- tle
         /*
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode*current = q.front();
-            q.pop();
-            if(isValid(current, LLONG_MIN, LLONG_MAX)){
-                int sum = find(current);
-                cout<<current->val<<" "<<sum<<endl;
-                ans = max(ans, sum);
-            }
-            if(current->left!=NULL) q.push(current->left);
-            if(current->right!=NULL) q.push(current->right);
-        }
-        return ans;
+        if(!root) return 0;
+        check(root);       
+        return ans; 
         */
 
-        // optimal -- using post order , left right, node
-        
+        // better --- tle
+        /*
+        if(!root) return 0;
+        check(root);       
+        return ans; 
+        */
+
+        // optimal ---
+        // do geek for geeks to understand -- post order traversal
         helper(root);
         return ans;
-        
     }
 };

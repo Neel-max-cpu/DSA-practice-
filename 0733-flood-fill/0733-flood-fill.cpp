1d49c008cd2334de53be0a7f559aa791eae7d090
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-06-06 08:36:17 +0530

    Time: 0 ms (100%), Space: 18 MB (90.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-    bool isValid(int r, int c, int n, int m, vector<vector<bool>>&visited, vector<vector<int>>&image, int current){
-        if(r<0 || r==n || c<0 || c==m || visited[r][c]==true || image[r][c]!=current) return false;
+    bool check(int newi, int newj, vector<vector<int>>&arr, int original, vector<vector<bool>>&visited){
+        int n = arr.size();
+        int m = arr[0].size();
+        if(newi >= n || newi<0 || newj >=m || newj<0) return false;
+        if(visited[newi][newj]) return false;
+        if(arr[newi][newj]!=original) return false;
         return true;
     }
-    void helper(vector<vector<int>>&image, int sr, int sc, int color, vector<vector<bool>>&visited, int n, int m){
-        
-        visited[sr][sc] = true;
-        int current = image[sr][sc];        
-        int dx[]={-1, 1, 0, 0};
-        int dy[]={0, 0, -1, 1};
+
+    void helper(vector<vector<int>>&arr, int row, int col, int color, vector<vector<bool>>&visited, int original, vector<vector<int>>&ans){
+        visited[row][col] = true;
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
         for(int i=0; i<4; i++){
-            int nr = sr+dx[i];
-            int nc = sc+dy[i];
-            if(isValid(nr, nc, n, m, visited, image, current)){
-                helper(image, nr, nc, color, visited, n, m);
+            int newi = row+dy[i];
+            int newj = col+dx[i];            
+            ans[row][col] = color;
+            if(check(newi, newj, arr, original, visited)){
+                helper(arr, newi, newj, color, visited, original, ans);
             }
         }
-        return;
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-        vector<vector<bool>>visited(n, vector<bool>(m, false));
-        vector<vector<int>>v(n, vector<int>(m));
-        helper(image, sr, sc, color, visited, n, m);
-        for(int i=0;i<n; i++){
-            for(int j=0; j<m; j++){
-                if(visited[i][j]==true)v[i][j]=color;
-                else v[i][j]=image[i][j];
-            }
-        }
-        return v;
+        vector<vector<bool>>visited(n, vector<bool>(m,false));
+        vector<vector<int>>ans = image;
+        int original = image[sr][sc];
+        helper(image, sr, sc, color, visited, original, ans);
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(!visited[i][j]){
+        //             original = image[i][j];
+        //             helper(image, i, j, color, visited, original, ans);
+        //         }
+        //     }
+        // }
+        return ans;
     }
 };

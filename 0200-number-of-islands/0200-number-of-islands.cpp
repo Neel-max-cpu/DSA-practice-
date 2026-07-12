822cf51d3b59ac57cb1bf2cd1dd430822282181d
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-07-12 22:49:49 +0530

    Time: 23 ms (84.21%), Space: 16.7 MB (62.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,57 +1,26 @@
 class Solution {
-public:    
-    void dfs(vector<vector<bool>>&visited, vector<vector<char>>&grid, int i, int j){
-        visited[i][j] = true;
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
-        int n = grid.size();
-        int m = grid[0].size();
-        for (int k = 0; k < 4; k++) {
-            int nrow = i + dx[k];
-            int ncol = j + dy[k];
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
-                grid[nrow][ncol] == '1' && !visited[nrow][ncol]) {
-                dfs(visited, grid, nrow, ncol);            
-            }
-        }
+public:
+
+    bool valid(int row, int col, int n, int m, vector<vector<char>>&grid, vector<vector<bool>>&visited){
+        if(row<0 || row>=n || col<0 || col>=m) return false;
+        if(grid[row][col]=='0') return false;
+        if(visited[row][col]) return false;
+        return true;
     }
 
-    void bfs(vector<vector<bool>>&visited, vector<vector<char>>&grid, int i, int j){
-        visited[i][j] = true;
-        queue<pair<int,int>>q;
-        q.push({i, j});
-        int n = grid.size();
-        int m = grid[0].size();
+    void dfs(int row, int col, int n, int m, vector<vector<char>>&grid, vector<vector<bool>>&visited){
+        visited[row][col] = true;
 
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
-        
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
+        // i can do in any 4 direction
+        // left, up, right, down
+        int dx[] = {-1,1,0,0};
+        int dy[] = {0,0,-1,1};
 
-            // here 8 direction searching wont' work
-            /*     
-            for(int ki = -1 ; ki<=1; ki++){
-                for(int kj= -1; kj<=1; kj++){
-                    int nrow = row+ki;
-                    int ncol = col+kj;
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visited[nrow][ncol] ){
-                        visited[nrow][ncol] = true;
-                        q.push({nrow, ncol});
-                    }
-                }
-            }
-            */
-            for (int k = 0; k < 4; k++) {
-                int nrow = row + dx[k];
-                int ncol = col + dy[k];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
-                    grid[nrow][ncol] == '1' && !visited[nrow][ncol]) {
-                    visited[nrow][ncol] = true;
-                    q.push({nrow, ncol});
-                }
+        for(int i=0; i<4; i++){
+            int newRow = row+dy[i];
+            int newCol = col+dx[i];
+            if(valid(newRow, newCol, n, m, grid, visited)){
+                dfs(newRow, newCol, n, m, grid, visited);
             }
         }
     }
@@ -60,15 +29,14 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<bool>>visited(n, vector<bool>(m, false));
+        
         int count = 0;
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+            for(int j = 0; j<m; j++){
                 if(!visited[i][j] && grid[i][j]=='1'){
+                    // dfs or bfs
+                    dfs(i,j, n, m, grid, visited);
                     count++;
-                    // bfs
-                    // bfs(visited, grid, i, j);
-                    // dfs
-                    dfs(visited, grid, i, j);
                 }
             }
         }

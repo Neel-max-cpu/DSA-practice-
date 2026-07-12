18c19d93c6f89f5b1e3decc0649c5f71f0903c79
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-07-12 22:55:11 +0530

    Time: 33 ms (20.96%), Space: 23.6 MB (26.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -24,6 +24,32 @@ public:
             }
         }
     }
+    
+    void bfs(int row, int col, int n, int m, vector<vector<char>>&grid, vector<vector<bool>>&visited){
+
+        queue<pair<int,int>>q;        
+        q.push({row, col});
+        visited[row][col] = true;
+
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+
+        while(!q.empty()){
+            auto currNode = q.front();
+            q.pop();
+            int currRow = currNode.first;
+            int currCol = currNode.second;            
+
+            for(int i=0; i<4; i++){
+                int newRow = currRow+dy[i];
+                int newCol = currCol+dx[i];
+                if(valid(newRow, newCol, n, m, grid, visited)){
+                    visited[newRow][newCol] = true;
+                    q.push({newRow, newCol});
+                }
+            }
+        }        
+    }
 
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
@@ -33,9 +59,10 @@ public:
         int count = 0;
         for(int i=0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
+                if(!visited[i][j] && grid[i][j]=='1'){                    
                     // dfs or bfs
-                    dfs(i,j, n, m, grid, visited);
+                    // dfs(i,j, n, m, grid, visited);
+                    bfs(i,j, n, m, grid, visited);
                     count++;
                 }
             }

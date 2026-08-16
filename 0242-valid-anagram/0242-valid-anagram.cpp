5f3c0c3787c937df8a46cc2ed28afce8a70fb8fe
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-16 16:57:58 +0530

    Time: 0 ms (100%), Space: 10 MB (11.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    bool isAnagram(string s, string t) { 
         if(s.size()!=t.size()) return false;
-
-        vector<int>v(26,0);
-        for(int i=0; i<s.size(); i++){
-            char c = s[i];
-            v[c-'a']++;
-        }
-        for(int i=0; i<t.size(); i++){
-            char c = t[i];
-            v[c-'a']--;
-        }
         
-        for(auto it:v){
-            if(it<0 || it>0) return false;
+        unordered_map<char,int>n;
+        unordered_map<char,int>m;
+        for(auto it:s){
+            n[it]++;
         }
-        return true;
+        for(auto it:t){
+            m[it]++;
+        }
+        if(m!=n) return false;
+        return true;        
     }
 };

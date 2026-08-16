d56ecadb19d6ab9e0f25163cf2fa854264a6cb76
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-16 21:19:43 +0530

    Time: 1 ms (65.48%), Space: 18.1 MB (45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& arr, int k) {
-        /*        
+    vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>m;
-        for(auto it:arr){
+        for(auto it:nums){
             m[it]++;
         }
 
@@ -11,35 +10,13 @@ public:
         for(auto it:m){
             pq.push({it.second, it.first});
         }
+
         vector<int>ans;
-        for(int i=0; i<k; i++){
-            auto val = pq.top();
+        while(k>0 && !pq.empty()){
+            k--;
+            auto x = pq.top();
+            ans.push_back(x.second);
             pq.pop();
-            ans.push_back(val.second);
-        }
-        return ans;
-        */
-
-        //follow up --
-        unordered_map<int,int>m;
-        for(auto it:arr){
-            m[it]++;
-        }
-        int n = arr.size();
-        vector<vector<int>>bucket(n+1);
-        // at max if all the elems are equal - then ele will have n freq so n+1;
-        for(auto it:m){
-            // put the ele in their freq if 1 has 3freq then 3 -> 1
-            //if 2 has 3 freq, then 3->1,2 ...
-            bucket[it.second].push_back(it.first);
-        }
-
-        vector<int>ans;
-        for(int i=n; i>=1 && ans.size()<k; i--){
-            for(int it:bucket[i]){
-                ans.push_back(it);
-                if(ans.size()==k) break;
-            }
         }
         return ans;
     }

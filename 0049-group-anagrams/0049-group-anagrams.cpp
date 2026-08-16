82ee3f1dfcf2eba227acc56df071dfbe1b61ff5b
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-16 17:23:34 +0530

    Time: 94 ms (5.06%), Space: 37.8 MB (5.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,10 +2,34 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<string,vector<string>>m;
+        // better --
+        /*
         for(auto it:strs){
             string s = it;
             sort(s.begin(), s.end());
             m[s].push_back(it);
+        }
+        */
+
+        // best to avoid sort --
+        for(auto it:strs){
+            vector<int>count(26,0);
+            for(auto x:it){
+                count[x-'a']++;
+            }
+            string key = to_string(count[0]);
+            for(int i=1; i<26; i++){
+                key += ','+to_string(count[i]);
+            }
+            // here we skipped the sorting (from better)--
+            /*
+            eg it = eat
+            then vector<int>count would be --
+            [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
+            and then key would be == 1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0
+            so we can direclty put the key in map and check each time and is better than sorting
+            */
+            m[key].push_back(it);
         }
         vector<vector<string>>ans;
         for(auto it:m){

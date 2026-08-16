5369ae45c0a1ce2828afdd5309876366fb1e36c3
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-16 21:25:41 +0530

    Time: 0 ms (100%), Space: 20.1 MB (7.87%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,17 +6,34 @@ public:
             m[it]++;
         }
 
+        vector<int>ans;
+        /*
         priority_queue<pair<int,int>>pq;
         for(auto it:m){
             pq.push({it.second, it.first});
         }
 
-        vector<int>ans;
         while(k>0 && !pq.empty()){
             k--;
             auto x = pq.top();
             ans.push_back(x.second);
             pq.pop();
+        }
+        return ans;
+        */
+
+        // best --
+        int n = nums.size();
+        vector<vector<int>>buckets(n+1);
+        for(auto it:m){
+            buckets[it.second].push_back(it.first);
+        }
+
+        for(int i=n; i>=1 && ans.size()<k; i--){
+            for(auto it:buckets[i]){
+                ans.push_back(it);
+                if(ans.size()==k) break;
+            }
         }
         return ans;
     }

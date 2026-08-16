7e91c3b35f2341cb2997e8380a59889d8d7cfd2a
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-16 16:51:49 +0530

    Time: 69 ms (55.91%), Space: 111.2 MB (52.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,26 +1,17 @@
 class Solution {
-public:
+public:    
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        /*
-        map<int,int>m;
+        unordered_set<int>st;        
         for(auto it:nums){
-            m[it]++;
-        }
-        for(auto it:m) {
-            if(it.second>=2) return true;
-        }
-        return false;
-        */
-
-        // method 2 --
-        unordered_set<int>s;
-        for(int i=0; i<n; i++){
-            if(s.find(nums[i])!=s.end()){
+            if(st.find(it)!=st.end()){
+                // if found --
                 return true;
             }
-            s.insert(nums[i]);
-        }
+            else {
+                // not found
+                st.insert(it);                                
+            }
+        }        
         return false;
     }
 };

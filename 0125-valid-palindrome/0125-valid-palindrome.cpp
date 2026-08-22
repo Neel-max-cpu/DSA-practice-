a8b914b870aae144282c7594cec8cb8ca4c15933
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-22 11:17:47 +0530

    Time: 1 ms (43.67%), Space: 10.3 MB (23.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        // METHOD 2 -- using alpha numeric - O(n)
-        int i = 0, j= s.size()-1;
-        while(i<j){
-            // isalnum - Alphanumeric characters include letters and numbers, will return true, else return false
-            while(!isalnum(s[i]) && i<j) i++;
-            while(!isalnum(s[j]) && i<j) j--;
-
-            if(tolower(s[i])!=tolower(s[j])) return false;
-            i++;
-            j--;
-        }       
-        return true; 
-
-        // METHOD 1 -- o(n + n)
-        /*
-        string t = "";
-        for(auto it:s){
-            if(it>='a' && it<='z'){
-                t+=it;
-            }
-            else if(it>='A' && it<='Z'){
-                t+=tolower(it);
-            }
-            else if(it>='0' && it<='9'){
-                t+=it;
-            }
-        }
-
-        int i = 0, j=t.size()-1;
-        while(i<j){
-            if(t[i++]!=t[j--]) return false;
+    bool checkPalindrome(string &res){
+        cout<<res<<endl;
+        int n = res.size();
+        for(int i=0; i<n/2; i++){
+            if(res[i]!=res[n-1-i]) return false;
         }
         return true;
-        */
+    }
+
+    bool isPalindrome(string s) {
+        string res;
+        for(int i=0; i<s.size(); i++){
+            char c = s[i];
+            if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+                res+=tolower(c);
+            }
+            if(c>='0' && c<='9') res+=c;
+        }
+
+        if(checkPalindrome(res)) return true;
+        return false;
     }
 };

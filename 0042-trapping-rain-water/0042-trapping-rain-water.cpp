fafd530602da44b89fc3f31fd8bed135db5c93ea
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-22 22:30:28 +0530

    Time: 0 ms (100%), Space: 26 MB (77.72%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:    
     int trap(vector<int>& arr) {
-        int n = arr.size();
+        int n = arr.size();      
+        int ans = 0;  
+        /*
         // wall greater on right
         vector<int>tallOnRight(n);
         tallOnRight[0] = arr[0];
@@ -18,13 +20,34 @@ public:
         for(auto it:tallOnRight) cout<<it<<" ";
         cout<<endl;
         for(auto it:tallOnLeft) cout<<it<<" ";
-        
-        int ans = 0;
+                
         for(int i=0; i<n; i++){
             int waterLevel = min(tallOnRight[i], tallOnLeft[i]);
             int waterAboveMe = waterLevel - arr[i];
             ans += waterAboveMe;
         }
+        */
+
+        // optimal --
+        int i = 0, j = n-1;
+        int tallOnLeft =arr[i], tallOnRight = arr[j];
+        while(i<j){
+            if(tallOnLeft <= tallOnRight){
+                i++;                
+                int waterLevel = tallOnLeft;
+                int waterAboveMe = waterLevel - arr[i];
+                if(waterAboveMe>0) ans += waterAboveMe;;                
+                tallOnLeft = max(tallOnLeft, arr[i]);
+            }            
+            else{
+                j--;                
+                int waterLevel = tallOnRight;
+                int waterAboveMe = waterLevel - arr[j];
+                if(waterAboveMe>0) ans += waterAboveMe;;                
+                tallOnRight = max(tallOnRight, arr[j]);
+            }
+        }
+
 
         return ans;
     }

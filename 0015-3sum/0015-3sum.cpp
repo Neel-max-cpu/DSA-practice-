968be54dce88092e77635f5743d43f661cf6ea10
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-22 11:44:43 +0530

    Time: 44 ms (74.26%), Space: 29.1 MB (73.24%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
-        //brute - 3loopso(n^3)
-        //better sort - 1 fixed then 2 sum(nlogn + n* (n-1 to 1 sum)~ n * log k - when inserting)
-        int n = arr.size();
         sort(arr.begin(), arr.end());
+
+        int n = arr.size();
         vector<vector<int>>ans;
         for(int i=0; i<=n-3; i++){
-            if(i>0 && arr[i]==arr[i-1]) continue;
-            else{
-                int j = i+1, k=n-1;
-                while(j<k){
-
-                    int sum = arr[i]+arr[j]+arr[k];
-                    if(sum==0) {
-                        ans.push_back({arr[i],arr[j], arr[k]});
-                        j++;k--;
-                        while(j<k && arr[j]==arr[j-1]) j++;
-                        while(k>j && arr[k]==arr[k+1]) k--;                        
-                    }
-                    else if(sum>0){
-                        k--;
-                    }
-                    else if(sum<0){
-                        j++;
-                    }
+            if(i!=0 && arr[i]==arr[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = arr[i]+arr[j]+arr[k];
+                if(sum==0){
+                    ans.push_back({arr[i], arr[j], arr[k]});
+                    k--; j++;
+                    while(j<k && arr[j]==arr[j-1]) j++;
+                    while(k>j && arr[k]==arr[k+1]) k--;
+                }
+                else if(sum>0){
+                    k--;                    
+                }
+                else if(sum<0){
+                    j++;                    
                 }
             }
         }

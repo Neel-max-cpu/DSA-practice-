7efe9c3ac5cc1bc1552a2d10e85142e70a160a89
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-23 11:32:57 +0530

    Time: 11 ms (28.91%), Space: 11.1 MB (19.54%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,107 +1,57 @@
 class Solution {
 public:
-    int ans = 0;
-    void helper(string s, int k, char c, int mx, int i){
-        if(i==s.size()){
-            ans = max(mx, ans);
-            return;
-        }
-
-        if(s[i]==c){
-            helper(s, k, c, mx+1, i+1);
-        }
-
-        if(s[i]!=c){
-            if(k>0){                            
-                helper(s, k-1, c, mx+1, i+1);
-            }
-            else{
-                ans = max(mx, ans);
-                helper(s, k, c, 0, i+1);
-            }
-        }
-
-    }
-
     int characterReplacement(string s, int k) {
-        int n = s.size();
-        // o(n2)
-        /*
         int ans = 0;
-        for(int i=0; i<n; i++){
+        int n = s.size();
+        // brute --
+        /*
+        for(int i=0; i<n;i++){
+            char c = s[i];
+            int temp = k;
             int count = 1;
-            int tempK=k;
-            for(int j=i+1; j<n; j++){                
-                if(s[i]==s[j] && i!=j) count++;
-                else if(tempK>0){
+            for(int j=i+1; j<n; j++){
+                if(s[j]==c) {
                     count++;
-                    tempK--;
                 }
-                else break;
+                else if(s[j]!=c && temp > 0){
+                    count++;
+                    temp--;
+                }    
+                else{
+                    break;
+                }            
             }
             ans = max(count, ans);
         }
         return ans;
         */
 
-        /*
-        aababba
-        aaaa
-        
-        */       
 
-        // recursion -- doesn't pass all tc
-        /*
-        for(int i=0; i<n; i++){
-            helper(s, k, s[i], 0, 0);
+        unordered_map<char,int>m;                
+        int maxFreq=0;
+        int left = 0;
+        for(int right = 0; right<n; right++){
+            char c = s[right];
+            // freq increase
+            m[c]++;
+
+            maxFreq = max(maxFreq, m[c]);
+            /*
+                windowSize = right-left+1;
+                basically window is valid if windowsize - maxFreq <= k
+                why :
+                ababba and k = 2
+                let l = 0 and r=1 so in map a=1, and b=1 and windowsize = r-l+1 = 2
+                and maxFreq = 1 so 2-1 = 1(number of replacements we have to make) which is <=k so valid
+                
+                if not valid then while loop(below)
+            */
+            while((right-left+1)-maxFreq > k){
+                m[s[left]]--;
+                left++;
+            }
+            ans = max(right-left+1, ans);
         }
         return ans;
-        */
-
-        // o(n*26) ~ o(n)
-        map<char,int>m;
-        int ans = 0;
-        int l=0, r=0;
-        while(r<n){
-            char c = s[r];
-            if(m.find(c)!=m.end()){
-                m[c]++;
-            }
-            else m[c] = 1;
-
-            // most freq char
-            int maxi = 0;
-            for(auto it:m){
-                maxi = max(maxi, it.second);
-            }
-            // size of the window
-            int size = r-l+1;
-            
-            // if valid
-            if(size-maxi<=k){
-                cout<<l<<" "<<r<<": ";
-                ans = max(ans, size);
-                cout<<ans<<endl;
-            }
-            else{
-                m[s[l]]--;
-                l++;
-            }            
-            r++;
-        }
-        return ans;
-        
-
-
-
-
-        /*
-        aababba
-        a a a a ans = max(3-0+1)
-        
-
-        */
-
-       
     }
 };

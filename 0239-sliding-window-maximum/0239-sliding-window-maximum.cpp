c98cf502554a099178a94d9106e4c529c47ca082
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-23 16:56:44 +0530

    Time: 24 ms (52.73%), Space: 139.2 MB (73.57%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -13,7 +13,7 @@ public:
         int n = arr.size();        
         vector<int>ans;
 
-        // brute --
+        // brute -- o(nk)
         /*
         for(int right=0; right<n; right++){
             if(right>=k-1){
@@ -23,6 +23,9 @@ public:
         }
         return ans;
         */        
+
+        // better -- o(nlogk)
+        /*
         priority_queue<pair<int,int>>pq;  
         for(int right = 0; right<n; right++){
                      
@@ -44,7 +47,27 @@ public:
             pq.pop();
         }
         auto x = pq.top();
-        ans.push_back(x.first);        
+        ans.push_back(x.first);   
+        */
+
+        // optimal --- 
+        // max elemenet in the front and smaller in the back(descending order)
+        // and if we get bigger element than we pop from the back and put it in place
+        deque<int>dq;
+
+        for(int i=0; i<n; i++){
+            //case1:  if outside the window pop from front since oldest element there
+            while(!dq.empty() && dq.front()+k-1 < i) dq.pop_front();
+
+            // case2: curr is greater -- pop from back since newest element in back
+            while(!dq.empty() && arr[i] >= arr[dq.back()]) dq.pop_back();
+
+            dq.push_back(i);
+
+            if(i>=k-1){
+                ans.push_back(arr[dq.front()]);
+            }
+        }     
 
         return ans;
     }

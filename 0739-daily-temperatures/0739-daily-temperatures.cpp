21ba9d942aae9bbe0973c6fd467f00c5f0cad341
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-24 19:57:15 +0530

    Time: 11 ms (94.8%), Space: 107.3 MB (46.85%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,6 +3,9 @@ public:
     vector<int> dailyTemperatures(vector<int>& arr) {
         int n = arr.size();
         vector<int>ans(n,0);
+
+        // good --
+        /*
         stack<pair<int,int>>st;
         for(int i=0; i<n; i++){                                        
             while(!st.empty() && st.top().first<arr[i]){
@@ -13,6 +16,20 @@ public:
                 ans[index] = days;
             }
             st.push({arr[i], i});             
+        }
+        return ans;
+        */
+
+        // little better -- not storing the temp again
+        stack<int>st;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && arr[st.top()]<arr[i]){
+                int index = st.top();
+                st.pop();
+                int days = i-index;
+                ans[index]=days;
+            }
+            st.push(i);
         }
         return ans;
 

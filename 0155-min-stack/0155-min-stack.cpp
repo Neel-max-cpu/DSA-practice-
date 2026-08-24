d4c68efa8d8106ec6b1b032dd0347510634bd172
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-24 08:11:42 +0530

    Time: 67 ms (31%), Space: 151.1 MB (79.77%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,12 +1,20 @@
 class MinStack {
 public:
-    stack<pair<int,int>>st;
-    int currMin;
-    MinStack() {
-        currMin = INT_MAX;
+    // METHOD 1: -- with statck and pair o(1)
+    stack<pair<int,int>>st;    
+
+    // METHOD 2: -- with stack and encoding o(1)
+    // stack<long>st;
+    // long currMin;
+    MinStack() { 
+        // METHOD 2
+        // currMin = LONG_MAX;       
     }
     
     void push(int value) {
+        // METHOD 1:
+        // /*
+        int currMin;
         if(st.empty()) currMin = value;        
         else {
             int topMin = st.top().second;
@@ -15,18 +23,63 @@ public:
         }
 
         st.push({value, currMin});
+        // */
+
+        // METHOD 2:
+        /*
+        if(st.empty()){
+            currMin = value;
+            st.push(value);
+            return;
+        }
+
+        if(value>currMin){
+            st.push(value);
+        }
+        else{
+            int newMin = value;
+            long encode = 2*(long)newMin - currMin;
+            st.push(encode);
+            currMin = newMin;
+        }
+        */
     }
     
     void pop() {
-        st.pop();     
+        // METHOD 1 :
+        st.pop();    
+
+        // METHOD 2 :
+        // long top = st.top();
+        // st.pop();
+        // if(top<currMin){
+        //     int encoded = top;
+        //     /*
+        //     encoded = 2*newMin - oldMin
+        //     oldMin = 2*newMin - encoded
+        //     */
+        //     long preMinElement = 2*currMin - encoded;
+        //     currMin = preMinElement;
+        // } 
+        // if(st.empty()) currMin = LONG_MAX;
     }
     
     int top() {
+        // METHOD 1:
         return st.top().first;
+
+        // METHOD 2:
+        // long top = st.top();
+        // if(top>currMin) return (int) top;
+        // else return (int) currMin;
     }
     
     int getMin() {
+        // METHOD 1:
         return st.top().second;
+
+        // METHOD 2:
+        // return (int) currMin;
     }
 };
 

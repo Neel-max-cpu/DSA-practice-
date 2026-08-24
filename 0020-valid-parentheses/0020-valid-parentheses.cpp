85258d652a050b0aea8bfc80e35d47a131f2c6f4
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-24 07:40:42 +0530

    Time: 3 ms (9.18%), Space: 8.7 MB (89.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,17 +4,25 @@ public:
         stack<char>st;
         for(auto it:s){
             if(it=='(' || it=='{' || it=='[') st.push(it);
-            else {
+            else if(it==')'){
                 if(st.empty()) return false;
-                char x = st.top();
-                if(it==')' && x=='(') st.pop();
-                else if(it=='}' && x=='{') st.pop();
-                else if(it==']' && x=='[') st.pop();
+                char c = st.top();
+                if(c=='(') st.pop();
+                else return false;
+            }
+            else if(it=='}'){
+                if(st.empty()) return false;
+                char c = st.top();
+                if(c=='{') st.pop();
+                else return false;
+            }
+            else if(it==']'){
+                if(st.empty()) return false;
+                char c = st.top();
+                if(c=='[') st.pop();
                 else return false;
             }
         }
-        if(st.empty())
-            return true;
-        return false;
+        return st.empty() ? true : false;
     }
 };

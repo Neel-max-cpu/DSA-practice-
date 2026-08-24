d28d6d8eb9b0c305abb6eab535180d9c59c6c228
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-24 08:00:51 +0530

    Time: 66 ms (33.47%), Space: 151 MB (79.77%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,81 +1,39 @@
 class MinStack {
 public:
-    stack<int>st;
-    
-    // brute ---
-    /*
-    priority_queue<int,vector<int>,greater<int>> pq;
-    */
-
-    // better ---
-    long minE;    
-    stack<int>mini;
+    stack<pair<int,int>>st;
+    int currMin;
     MinStack() {
-        // very large value
-        minE = LONG_MAX;
+        currMin = INT_MAX;
     }
     
-    void push(int val) {
-        // brute ---
-        /*        
-        pq.push(val);
-        */
-        minE = min(minE, (long)val);            
-        st.push(val);
-        mini.push(minE);
+    void push(int value) {
+        if(st.empty()) currMin = value;        
+        else {
+            int topMin = st.top().second;
+            if(value<topMin) currMin = value;
+            else currMin = topMin;
+        }
+
+        st.push({value, currMin});
     }
     
     void pop() {
-        // brute ---
-        /*
-        int x = st.top();
-        popQ(x);        
-        */
-        st.pop();
-        mini.pop();
-        if(mini.empty()) minE = LONG_MAX;        
-        else{
-            minE = mini.top();
-        }
+        st.pop();     
     }
     
-    int top() {                
-        if(st.empty()) return -1;
-        else return st.top();           
+    int top() {
+        return st.top().first;
     }
     
     int getMin() {
-        // brute ---
-        /*
-        if(pq.empty()) return -1;
-        else return pq.top();
-        */
-        if(mini.empty()) return -1;
-        else return mini.top();
+        return st.top().second;
     }
-
-    // brute ---
-    /*
-    private:
-    void popQ(int x){
-        vector<int>temp;
-        while(pq.top()!=x){
-            temp.push_back(pq.top());
-            pq.pop();
-        }
-
-        // pop the ele
-        pq.pop();
-
-        for(auto it:temp) pq.push(it);
-    }
-    */
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(val);
+ * obj->push(value);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();

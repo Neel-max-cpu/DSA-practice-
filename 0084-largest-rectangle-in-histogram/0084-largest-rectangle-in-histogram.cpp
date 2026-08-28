8881d869ed674afb07572cb19459d3ee35771ee5
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-08-28 10:04:28 +0530

    Time: 23 ms (54.06%), Space: 85.9 MB (24.35%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,16 +1,21 @@
 class Solution {
-public:    
-    stack<int>putAllInStack(vector<int>&arr){
-        stack<int>st;
-        for(auto it:arr) st.push(it);
-        return st;
-    }
-
+public:
     int largestRectangleArea(vector<int>& arr) {
         int n = arr.size();
-        int ans = 0;
+        
+        /*
+        the question is not about how long i could stretch a historgam, its that is it viable to stretch
+        eg -- 2,1,5,6,2,3
 
-        // brute --
+        i = 0, height = 2 and area = 2
+        i = 1, height = 1 and current area = 1 (dont stretch i = 0 to 1 but try to stretch 1 to left or right) 
+
+        if for any i at any point going left or right (j, k where j<i and i<k) value is smaller than i
+        i.e arr[i]>arr[j] {where 0<=j<i} or arr[i]>arr[k] {where i<k<n} then break/cant go there
+        -- best eg // brute -- o(n2) finding left/right take o(n2) -- try dryrunning
+        */
+
+        // brute --        
         /*
         for(int i=0; i<n; i++){
             int mini = arr[i];
@@ -24,75 +29,110 @@ public:
         return ans;
         */
 
-        // better - easier understanding than  best(same thing just here sc - o(3n) there o(n))        
-        stack<int>st;
-        // save index
-        vector<int>smallLeft(n);
-        vector<int>smallRight(n);
-        
-        // for left
-        for(int i=0; i<n; i++){                        
-            while(!st.empty() && arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            // save index
-            if(!st.empty()) smallLeft[i] = st.top();
-            // if not available then can exted to the leftmost side (beyond 0th index)
-            else smallLeft[i] = -1;
 
-            st.push(i);            
+        // brute -- o(n2) finding left/right take o(n2)
+        /*
+        // from i nearest smallest element in the left if not there then can go till end
+        vector<int>left(n,-1);
+        // from i nearest smallest element in the right if not there then can go till end
+        vector<int>right(n,n);
+        for(int i=0; i<n; i++){
+            // for left
+            int val = arr[i];
+            for(int j=i-1; j>=0 ;j--){
+                if(val>arr[j]) {
+                    // have a smaller element so cant go left
+                    left[i] = j;
+                    break;
+                }
+            }
+            // for right
+            for(int j=i+1; j<n; j++){
+                if(val>arr[j]){
+                // have a smaller element so cant go right                
+                    right[i] = j;
+                    break;
+                }
+            }
         }
 
-        // empty the stack - fastest why --assigning a new empty stack
-        st = stack<int>();
+        // for(auto it:left){
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl;
+        // for(auto it:right){
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl;
 
-        // for right
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && arr[st.top()]>=arr[i]){
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            int width = right[i]-left[i]-1;
+            int area = arr[i]*width;
+            ans = max(area, ans);
+        }
+        return ans;
+        */
+    
+        // optimal -- finding the left/right using stack -- increasing height stack
+        // /*
+        vector<int>left(n,-1);
+        vector<int>right(n,n);
+
+        stack<int>st;
+        for(int i=0; i<n; i++){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
                 st.pop();
             }
-            // save index
-            if(!st.empty()) smallRight[i] = st.top();
-            // if not available then can exted to the rightmost side (beyond n-1th index)
-            else smallRight[i] = n;
+            if(!st.empty()) left[i] = st.top();
+            else left[i] = -1;
 
-            st.push(i);            
-        }        
+            st.push(i);
+        }
+
+        st = stack<int>();
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
+            if(!st.empty()) right[i] = st.top();
+            else right[i] = n;
+
+            st.push(i);
+        }
+
+        int ans = 0;
         for(int i=0; i<n; i++){
-            /* from (smallLeft[i] + 1)  to  (smallRight[i] - 1) = (sR-1)-(sL+1)=sR-sL-1
-            [since cant include the walls themselves - eg in [2,1,5,6,2,3] for wall 6
-            sL=5(idx 2) and sR=2(idx 4) so (4-1)-(2+1)+1 = 3-3+1 = 1 = which is width 1]             
-            */
-            // caues minimun left size -1 and max right size n =             
-            int width = smallRight[i]-smallLeft[i]-1;
+            int width = right[i]-left[i]-1;
             int area = arr[i]*width;
             ans = max(ans, area);
         }
         return ans;
+        // */
 
-        // best --- same as above but with one pass
-        /*
-        stack<int>st;
-        // loop including the nth bar(doen't exist)
-        for(int i=0; i<=n; i++){
-            // current is smaller than the top or we reached then end(n)[if i==n we pop all and check]
-            while(!st.empty() && (i==n || arr[st.top()]>=arr[i])){
-                int height = arr[st.top()];
-                st.pop();
+        // best single pass -- 9but see the above one its better understanding)
+        // stack<int>st;
+        // int ans = 0;
+        // for(int i=0; i<=n; i++){
+        //     while(!st.empty() && (i==n || arr[st.top()] >= arr[i])){
+        //         // bar the stack top found its right boundary -- so compute area
+        //         /* do dry run will understand but optimal is better --
+        //         [2,1,5,6,2,3]
+        //         i = 4; 6>=2 so right boundary of i=3(current stack top) is i-1(since arr[i]<=arr[stack top])
+        //         and left boundary will be current stack.top() after pop 
+        //         so width = (i - 1) - (st.top() + 1) + 1 = i-stack.top()-1
+        //         if(stack.empty()) then width = i (since can extend till end - 0 to i-1 so total = i)
+        //         then area = widht * arr[stack.top()]
+        //         */
 
-                int width;
-                if(st.empty()){
-                    width = i;  // length can be whole basically (i-1)-0+1 = i
-                }                
-                else width = i-st.top()-1;
-                int area = width*height;                
-                ans = max(ans, area);
-            }
-            st.push(i);
-        }              
-        return ans;  
-        */
-        
-        
+        //         int height = arr[st.top()];
+        //         st.pop();
+        //         int width = 0;
+        //         if(st.empty()) width = i;
+        //         else width = i-st.top()-1;
+        //         int area = width * height;
+        //         ans = max(ans, area);
+        //     }
+        //     st.push(i);
+        // }
+        // return ans;
     }
 };

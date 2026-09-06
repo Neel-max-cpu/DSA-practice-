08d73c667d6d1fae59697736a2fdfa36db104ecb
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-09-06 08:59:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,26 +2,57 @@ class Solution {
 public:
     int search(vector<int>& arr, int target) {
         int n = arr.size();
-        int low = 0, high = n-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(arr[mid]==target) return mid;
-            // if left part is sorted
-            if(arr[low]<=arr[mid]){
-                //left sorted
-                if(arr[low]<=target && target<arr[mid]){
-                    high = mid-1;
-                }
-                else low = mid+1;
+        int left = 0, right=n-1;
+        int ans = -1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(arr[mid]==target){
+                ans = mid;
+                return ans;
             }
-            else if(arr[mid]<=arr[high]){
-                //right sorted
-                if(arr[mid]<target && target<=arr[high]){
-                    low = mid+1;
+            else if(arr[mid]>arr[right]){
+                // here right side sorted in smaller, and left side sorted in bigger                
+                if(arr[mid]>target){
+                    if(target<=arr[right]){
+                        // need to find in smller half - right
+                        left = mid+1;
+                    }
+                    else{
+                        right = mid-1;
+                    }
                 }
-                else high = mid-1;
+                else{
+                    if(target>=arr[left]){
+                        // need to find in larger half - left                        
+                        right = mid-1;
+                    }
+                    else{
+                        left = mid+1;                        
+                    }
+                }                
+            }
+            else{                
+                // here left side sorted in smaller, and right side sorted in bigger                
+                if(arr[mid]>target){
+                    if(target>=arr[left]){
+                        // need to find in smller half - left
+                        right = mid-1;
+                    }
+                    else{
+                        left = mid+1;
+                    }                    
+                }
+                else{
+                    if(target<=arr[right]){
+                        // need to find in larger half - right
+                        left = mid+1;                        
+                    }
+                    else{
+                        right = mid-1;
+                    }                    
+                }                
             }
         }
-        return -1;
+        return ans;
     }
 };

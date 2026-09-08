e73d68bdd8c4ef6c0c1061d2d5451e7e94f7e0e2
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-09-08 22:00:01 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.44%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,79 +11,14 @@
  */
 class Solution {
 public:
-    bool helper(TreeNode *p, TreeNode*q){
-        // check both null
-        if(p==NULL && q==NULL) return true;
-
-        // only one is null (cause both null check above)
-        if(p==NULL || q==NULL) return false;
-
-        // now both p and q are present -- so check data, move left, move right
-        return ((p->val == q->val) && helper(p->left, q->left) && helper(p->right, q->right));
-    } 
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // recurisive ---
-        // return helper(p, q);
+        if(!p && !q) return true;
+        else if(!p || !q) return false;
 
-        // iterative --- (recursion better)
-        queue<TreeNode*>one;
-        queue<TreeNode*>two;
+        if(p->val != q->val) return false;
 
-        if(p) one.push(p);
-        if(q) two.push(q);
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 
-        while(!one.empty() && !two.empty()){
-            int len1 = one.size();
-            int len2 = two.size();
 
-            if(len1 != len2) {
-                cout<<"size not same!"<<endl;
-                return false;
-            }
-            for(int i=0; i<len1; i++){
-                TreeNode *x = one.front();
-                TreeNode *y = two.front();
-                one.pop();
-                two.pop();
-
-                // check data
-                if(x->val != y->val) return false;
-
-                if(x->left || y->left){
-                    // both null no problem
-                    if(!x->left && !y->left) continue;
-                    else if(!x->left || !y->left){
-                        // if one of them is null
-                        return false;
-                    }
-                    else{
-                        // both present --
-                        one.push(x->left);
-                        two.push(y->left);
-                    }
-                }
-
-                if(x->right || y->right){
-                    // both null no problem
-                    if(!x->right && !y->right) continue;
-                    else if(!x->right || !y->right){
-                        // if one of them is null
-                        return false;
-                    }
-                    else{
-                        // both present --
-                        one.push(x->right);
-                        two.push(y->right);
-                    }
-                }
-            }
-        }
-
-        if(!one.empty() || !two.empty()) {
-            cout<<"one/two is not empty!"<<endl;
-            return false;
-        }
-        else return true;
     }
 };

b65661524ee10a22a53e8d142a0e2f033723146a
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-09-18 10:34:51 +0530

    Time: 0 ms (100%), Space: 13.5 MB (40.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,67 +8,19 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include<vector>
-#include<algorithm>
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        // brute ---
-        /*
-        ListNode *temp = head;
-        vector<int>v;
-        while(temp!=NULL){
-            v.push_back(temp->val);
-            temp = temp->next;
-        }
-
-        // reverse(v.begin(), v.end());
-        ListNode *newHead = NULL;
-        ListNode *tail = NULL;
-        // for(int i=0; i<v.size(); i++){
-        for(int i=v.size()-1; i>=0; i--){
-            ListNode *newNode = new ListNode(v[i]);
-            if(newHead == NULL){
-                newHead = newNode;
-                tail = newNode;
-            }
-            else{
-                tail->next = newNode;
-                tail = tail->next;
-            }            
-        }
-        return newHead;
-        */
-
-        // better(best ---)
-        // recursion --
-        /*
-        if(head==NULL || head->next==NULL) return head;
-
-        ListNode *newhead = reverseList(head->next);
-        ListNode *front = head->next;
-        front->next = head;
-        head->next = NULL;
-
-        return newhead;
-        */
-
-        // best ---
-        // /*
-        ListNode *current = head;
-        ListNode *next = NULL;
-        ListNode *prev = NULL;
-
-        while(current != NULL){
-            next = current -> next;
-            current -> next = prev;
-            prev = current;
-            current = next;
-        }
-        head = prev;
-        return head;
-          // */
-
+    ListNode* reverseList(ListNode* head) {              
+        ListNode *prev = NULL;              
+        ListNode*curr = head;
+        // while(curr!= NULL){            
+        while(curr){
+            ListNode*next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }  
+        return prev;
 
     }
 };

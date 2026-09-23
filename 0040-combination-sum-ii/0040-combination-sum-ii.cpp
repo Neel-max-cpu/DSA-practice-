b151ceeb20e28109ba95ab7fcdf7ed2e5a12af64
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-09-23 22:34:44 +0530

    Time: 1 ms (48.99%), Space: 14.1 MB (63.33%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,68 +1,25 @@
 class Solution {
 public:
-
-    void helper(vector<int>&a, int target, vector<int>&ds, set<vector<int>>&ans, int i){
-        // base case
-        if(i==a.size()){
-            if(target==0){
-                // ans.push_back(ds);
-                ans.insert(ds);
-            }
+    void helper(vector<int>&arr, int target, int idx, vector<vector<int>>&ans, vector<int>&temp){
+        if(target==0){
+            ans.push_back(temp);
             return;
         }
 
-        // pick the ele
-        if(a[i]<=target){
-            ds.push_back(a[i]);
-            helper(a, target-a[i], ds, ans, i+1);
-            ds.pop_back();
+        for(int i=idx; i<arr.size(); i++){
+            if(i!=idx && arr[i]==arr[i-1]) continue;
+            if(arr[i]>target) break;
+            temp.push_back(arr[i]);
+            helper(arr, target-arr[i], i+1, ans, temp);
+            temp.pop_back();
         }
 
-        // dont pick the ele
-        helper(a, target, ds, ans, i+1);
     }
-
-    void helper2(vector<int>&a, int target, vector<int>&ds, vector<vector<int>>&ans, int idx){
-        
-        if(target==0) {
-            ans.push_back(ds);
-            return;
-        }
-
-        for(int i=idx; i<a.size(); i++){
-            if(i!=idx && a[i]==a[i-1]) continue;
-            // if(a[i]<=target){
-            //     ds.push_back(a[i]);
-            //     helper2(a, target-a[i], ds, ans, i+1);
-            //     ds.pop_back();
-            // }
-
-            // or 
-            if(a[i]>target) break;
-            ds.push_back(a[i]);
-            helper2(a, target-a[i], ds, ans, i+1);
-            ds.pop_back();
-
-        }
-    }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-
-        // using set tle --
-        /*
-        set<vector<int>>ans;
-        vector<int>ds;
-        sort(candidates.begin(), candidates.end());
-        helper(candidates, target, ds, ans, 0);
-        vector<vector<int>>res;
-        res.insert(res.end(),ans.begin(), ans.end());
-        return res; 
-        */
-
-        sort(candidates.begin(), candidates.end());
         vector<vector<int>>ans;
-        vector<int>ds;
-        helper2(candidates, target, ds, ans, 0);
+        vector<int>temp;
+        sort(candidates.begin(), candidates.end());
+        helper(candidates, target, 0, ans, temp);
         return ans;
     }
 };

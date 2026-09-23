6a8fadec74aebf02ce7466b202ab35b7b419d624
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-09-23 23:26:12 +0530

    Time: 0 ms (100%), Space: 102.4 MB (38.48%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minOperations(vector<int>& arr, int x) {
+        int n = arr.size();
+        int totalSum = 0;
+        for(auto it:arr) totalSum+=it;
+
+        int toLeave = totalSum-x;
+        if(toLeave<0) return -1;
+        if(toLeave == 0) return n;
+        // find the longes subarray with sum toLeave
+        int i = 0, j = 0;
+        int maxLen = 0;
+        int currSum = 0;
+        while(j<n){
+            currSum+=arr[j];                        
+            while(currSum>toLeave && i<j){
+                currSum-=arr[i++];
+            }
+            if(currSum==toLeave){
+                int len = j-i+1;
+                maxLen = max(maxLen, len);
+            }
+                        
+            j++;
+        }
+        return maxLen == 0 ? -1 : n-maxLen; 
+    }   
+};
